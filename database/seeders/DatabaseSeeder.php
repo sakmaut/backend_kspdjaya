dfd3fa29f2c76dd95638d9345ba00225c15c8fb7
alex <alexanderlieepm@gmail.com>
2024-05-14 08:07:30 +0700

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,9 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        \App\Models\User::factory(2)->create();
-        
+    { 
         DB::table('users')->insert([
+           [
             'username' => "frontend",
             'employee_id' => '03bf4f5c-4eac-11e9-b250-e0d55e0ad3ad',
             'email' => 'frontend@gmail.com',
@@ -26,6 +25,16 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('frontend'),
             'remember_token' => Str::random(10),
             'status' => "Active",
+           ],
+           [
+            'username' => "backend",
+            'employee_id' => '03bf4f5c-4eac-11e9-b250-e0d55e0ad3ad',
+            'email' => 'backend@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('backend'),
+            'remember_token' => Str::random(10),
+            'status' => "Active",
+           ],
         ]);
     }
 }
