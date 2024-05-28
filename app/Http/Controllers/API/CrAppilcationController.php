155676c79997ee94d70ead178d972e99c8426436
alex <alexanderlieepm@gmail.com>
2024-05-28 11:29:39 +0700

    update

--- app/Http/Controllers/API/CrAppilcationController.php
+++ app/Http/Controllers/API/CrAppilcationController.php
@@ -7,6 +7,7 @@ use App\Models\M_CrApplication;
 use App\Models\M_CrApplicationBank;
 use App\Models\M_CrGuaranteVehicle;
 use App\Models\M_CrPersonal;
+use App\Models\M_CrPersonalExtra;
 use App\Models\M_CrProspect;
 use App\Models\M_CrProspectDocument;
 use App\Models\M_HrEmployee;
@@ -275,6 +276,8 @@ class CrAppilcationController extends Controller
         $approval_detail = M_ProspectApproval::where('CR_PROSPECT_ID',$prospect_id)->first();
         $attachment_data = M_CrProspectDocument::where('CR_PROSPECT_ID',$prospect_id )->get();
         $cr_personal = M_CrPersonal::where('APPLICATION_ID',$application->ID)->first();
+        $cr_personal_extra = M_CrPersonalExtra::where('APPLICATION_ID',$application->ID)->first();
+        $cr_application_bank = M_CrApplicationBank::where('APPLICATION_ID',$application->ID)->first();
         
         $arrayList = [
             'id_application' => $application->ID,
@@ -282,26 +285,26 @@ class CrAppilcationController extends Controller
                 "data_pribadi" =>
                   [
                       "code" => $cr_personal->CUST_CODE??null,
-                      "nama" => $cr_personal->NAME??null,
+                      "nama" => $data->nama,
                       "nama_panggilan" => $cr_personal->ALIAS??null,
                       "jenis_kelamin" => $cr_personal->GENDER??null,
                       "tempat_lahir" => $cr_personal->BIRTHPLACE??null,
-                      "tanggal_lahir" => $cr_personal->BIRTHDATE??null,
+                      "tanggal_lahir" => date('d-m-Y',strtotime($data->tgl_lahir)),
                       "status_kawin" => $cr_personal->MARTIAL_STATUS??null,
                       "tanggal_kawin" => $cr_personal->MARTIAL_DATE??null,
                       "indentitas"  => [
                         "tipe" => $cr_personal->ID_TYPE??null,
-                        "no" => $cr_personal->ID_NUMBER??null,
+                        "no" => $data->ktp,
                         "tgl_terbit" => $cr_personal->ID_ISSUE_DATE??null,
                         "masa_berlaku" => $cr_personal->ID_VALID_DATE??null,
-                        "alamat" => $cr_personal->ADDRESS??null,
-                        "rt" => $cr_personal->RT??null,
-                        "rw" => $cr_personal->RW??null,
-                        "provinsi" => $cr_personal->PROVINCE??null,
-                        "kota" => $cr_personal->CITY??null,
-                        "kelurahan" => $cr_personal->KELURAHAN??null,
-                        "kecamatan" => $cr_personal->KECAMATAN??null,
-                        "kode_pos" => $cr_personal->ZIP_CODE??null
+                        "alamat" => $data->alamat,
+                        "rt" => $data->rt,
+                        "rw" => $data->rw,
+                        "provinsi" => $data->province,
+                        "kota" => $data->city,
+                        "kelurahan" => $data->kelurahan,
+                        "kecamatan" => $data->kecamatan,
+                        "kode_pos" => $data->zip_code
                       ],
                       "no_kk" => $cr_personal->KK??null,
                       "warganegara" => $cr_personal->CITIZEN??null,
@@ -311,68 +314,104 @@ class CrAppilcationController extends Controller
                       "pendidikan" => $cr_personal->EDUCATION??null,
                       "status_rumah" => $cr_personal->PROPERTY_STATUS??null,
                       "telepon_rumah" => $cr_personal->PHONE_HOUSE??null,
-                      "telepon_selular" => $cr_personal->PHONE_PERSONAL??null,
+                      "telepon_selular" => $data->hp,
                       "telepon_kantor" => $cr_personal->PHONE_OFFICE??null,
                       "ext1" => $cr_personal->EXT_1??null,
                       "ext2" => $cr_personal->EXT_2??null
                   ],
                   "alamat_tagih"  => [
-                    "alamat" => "Jl.Bahagia",
-                    "rt" => "002",
-                    "rw" => "010",
-                    "provinsi" => "Jawa tenggara",
-                    "kota" => "jawa",
-                    "kelurahan" => "Pamulang",
-                    "kecamatan" => "apa?",
-                    "kode_pos" => "215458"
+                    "alamat" => $cr_personal->INS_ADDRESS??null,
+                    "rt" => $cr_personal->INS_RT??null,
+                    "rw" => $cr_personal->INS_RW??null,
+                    "provinsi" => $cr_personal->INS_PROVINCE??null,
+                    "kota" => $cr_personal->INS_CITY??null,
+                    "kelurahan" => $cr_personal->INS_KELURAHAN??null,
+                    "kecamatan" => $cr_personal->INS_KECAMATAN??null,
+                    "kode_pos" => $cr_personal->INS_ZIP_CODE??null
                   ]
             ],
-                
-            'id' => $prospect_id,
-            'cust_code_ref' => $data->cust_code_ref,
             'data_order' =>[
-                'tujuan_kredit' => $data->tujuan_kredit,
-                'plafond' => 'IDR '.number_format($data->plafond,0,",","."),
-                'tenor' => $data->tenor, 
-                'kategory' => $data->category 
+                'cr_prospect_id' => $prospect_id,
+                "nama_ibu" => $data->work_period, 
+                "kategori" => $data->category, 
+                "gelar" => $data->title, 
+                "lama_bekerja" => $data->mother_name, 
+                "tanggungan" => $data->dependants, 
+                "biaya_bulanan" => $data->expense, 
+                "pendapatan_pribadi" => $data->income_personal,
+                "pendapatan_pasangan" => $data->income_spouse,
+                "pendapatan_lainnya" => $data->income_other,
+                "order" => [
+                    "tanggal" => "",
+                    "status" => "",
+                    "tipe" => ""
+                ],
+                "unit_bisnis" => "", 
+                "cust_service" => "",
+                "ref_pelanggan" => $data->cust_code_ref,
+                "surveyor_id" => "",
+                "catatan_survey" => $data->survey_note,
+                "prog_marketing" => "",
+                "cara_bayar" => ""
             ],
-            'data_nasabah' => [
-                'nama' => $data->nama,
-                'tgl_lahir' => date('d-m-Y',strtotime($data->tgl_lahir)),
-                'no_hp' => $data->hp,
-                'no_ktp' => $data->ktp,
-                'alamat' => $data->alamat,
-                'rt' => $data->rt,
-                'rw' => $data->rw,
-                'provinsi' => $data->province,
-                'kota' => $data->city,
-                'kelurahan' => $data->kelurahan,
-                'kecamatan' => $data->kecamatan,
-                'kode_pos' => $data->zip_code
-            ], 
-            'data_survey' =>[
-                'usaha' => $data->usaha,
-                'sektor' => $data->sector,
-                'lama_bekerja' => $data->work_period,
-                'tanggungan' => $data->dependants,
-                'pengeluaran' => $data->expenses,
-                'penghasilan_pribadi' => $data->income_personal,
-                'penghasilan_pasangan' => $data->income_spouse,
-                'penghasilan_lainnya' => $data->income_other,
-                'tgl_survey' => is_null($data->visit_date) ? '': date('d-m-Y',strtotime($data->visit_date)),
+            "data_pelanggan_tambahan" => [
+                "nama_bi"  => $cr_personal_extra->BI_NAME ?? null, 
+                "email"  => $cr_personal_extra->EMAIL?? null,
+                "info_khusus"  => $cr_personal_extra->INFO?? null,
+                "usaha_lain_1"  => $cr_personal_extra->OTHER_OCCUPATION_1?? null,
+                "usaha_lain_2"  => $cr_personal_extra->OTHER_OCCUPATION_2?? null,
+                "usaha_lain_3"  => $cr_personal_extra->OTHER_OCCUPATION_3?? null,
+                "usaha_lain_4"  => $cr_personal_extra->OTHER_OCCUPATION_4?? null,
+                "surat"  => [
+                  "alamat"  => $cr_personal_extra->MAIL_ADDRESS?? null,
+                  "rt"  => $cr_personal_extra->MAIL_RT?? null,
+                  "rw"  => $cr_personal_extra->MAIL_RW?? null,
+                  "provinsi" => $cr_personal_extra->MAIL_PROVINCE?? null,
+                  "kota" => $cr_personal_extra->MAIL_CITY?? null,
+                  "kelurahan" => $cr_personal_extra->MAIL_KELURAHAN?? null,
+                  "kecamatan" => $cr_personal_extra->MAIL_KECAMATAN?? null,
+                  "kode_pos" => $cr_personal_extra->MAIL_ZIP_CODE?? null
+                ] ,
+                "kerabat_dalam_kondisi_darurat"  => [
+                    "nama"  => $cr_personal_extra->EMERGENCY_NAME?? null,
+                    "alamat"  => $cr_personal_extra->EMERGENCY_ADDRESS?? null,
+                    "rt"  => $cr_personal_extra->EMERGENCY_RT?? null,
+                    "rw"  => $cr_personal_extra->EMERGENCY_RW?? null,
+                    "provinsi" =>$cr_personal_extra->EMERGENCY_PROVINCE?? null,
+                    "kota" => $cr_personal_extra->EMERGENCY_CITY?? null,
+                    "kelurahan" => $cr_personal_extra->EMERGENCY_KELURAHAN?? null,
+                    "kecamatan" => $cr_personal_extra->EMERGENCY_KECAMATAN?? null,
+                    "kode_pos" => $cr_personal_extra->EMERGENCY_ZIP_CODE?? null,
+                    "no_telp" => $cr_personal_extra->EMERGENCY_PHONE_HOUSE?? null,
+                    "no_hp" => $cr_personal_extra->EMERGENCY_PHONE_PERSONAL?? null
+                ],      
             ],
+            "bank" =>[],
+            "jaminan_kendaraan" => [],
             "lokasi" => [ 
-                'coordinate' => $data->coordinate,
-                'accurate' => $data->accurate
+                "coordinate" => $data->coordinate,
+                "accurate" => $data->accurate
             ],         
-            'jaminan_kendaraan' => [],
-            'prospect_approval' => [
-                'flag_approval' => $approval_detail->ONCHARGE_APPRVL,
-                'keterangan' => $approval_detail->ONCHARGE_DESCR,
-                'status' => $approval_detail->APPROVAL_RESULT
+            "jaminan_kendaraan" => [],
+            "prospect_approval" => [
+                "flag_approval" => $approval_detail->ONCHARGE_APPRVL,
+                "keterangan" => $approval_detail->ONCHARGE_DESCR,
+                "status" => $approval_detail->APPROVAL_RESULT
             ],
             "attachment" =>$attachment_data
         ];
+        
+        if(!empty($cr_application_bank) && is_array($cr_application_bank)){
+            foreach ($cr_application_bank as $list) {
+                $arrayList['bank'][] = [
+                    "kode_bank" => $list['BANK_CODE'] ?? null,
+                    "nama_bank" => $list['BANK_NAME'] ?? null,
+                    "no_rekening" => $list['ACCOUNT_NUMBER'] ?? null,
+                    "nama_di_rekening" => $list['ACCOUNT_NAME'] ?? null,
+                    "status" => $list['STATUS'] ?? null
+                ];    
+            }
+        }
 
         foreach ($guarente_vehicle as $list) {
             $arrayList['jaminan_kendaraan'][] = [
