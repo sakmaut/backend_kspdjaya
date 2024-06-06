4ba3c6cf42c95b6a8e8c5f62a80491b43ce3cf2f
alex <alexanderlieepm@gmail.com>
2024-06-06 13:46:36 +0700

    updat

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'paths' => ['*', 'sanctum/csrf-cookie'],
+    'paths' => ['/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080/', 'https://api.kspdjaya.id/'],
+    'allowed_origins' => ['http://localhost:8080', 'https://api.kspdjaya.id'],
 
     'allowed_origins_patterns' => [],
 
