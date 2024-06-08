272dfbf64b95892da24072a4cde72b58c659116a
alex <alexanderlieepm@gmail.com>
2024-06-08 08:31:53 +0700

    updated

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HandleCors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, X-Requested-With, Authorization');
+        $response->header('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
