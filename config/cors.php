28abfd995e104e33645f4fbf53dde81cf66d9bda
alex <alexanderlieepm@gmail.com>
2024-06-08 08:57:30 +0700

    updated

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://los.kspdjaya.id'],
 
     'allowed_origins_patterns' => [],
 
