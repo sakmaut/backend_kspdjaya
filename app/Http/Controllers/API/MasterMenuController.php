4518d20e4514695232080f47d51e39ea2b3af0fd
alex <alexanderlieepm@gmail.com>
2024-06-21 13:30:51 +0700

    update

--- app/Http/Controllers/API/MasterMenuController.php
+++ app/Http/Controllers/API/MasterMenuController.php
@@ -22,6 +22,7 @@ class MasterMenuController extends Controller
                                         $query->whereNull('deleted_by')
                                             ->orWhere('deleted_by', '');
                                     })
+                                    ->where('menu_name','home')
                                     ->whereNotNull('parent')
                                     ->where('parent', '!=', '')
                                     ->orderBy('order', 'asc')
