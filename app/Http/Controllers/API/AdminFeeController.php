d8ceae9701c25016c561065d59a548e89dc73115
alex <alexanderlieepm@gmail.com>
2024-07-10 11:35:33 +0700

    update

--- app/Http/Controllers/API/AdminFeeController.php
+++ app/Http/Controllers/API/AdminFeeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\M_AdminFee;
 use App\Models\M_AdminType;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -16,47 +18,28 @@ class AdminFeeController extends Controller
     {
         try {
             $data = M_AdminFee::with('links')->get();
+            $show = $this->buildArray($data);
 
-            $build = [];
-        
-            foreach ($data as $value) {
-              
-                $struktur = [];
-
-                foreach ($value->links as $link) {
-                    $struktur[] = [
-                        'fee_name' => $link['fee_name'],
-                        '6_month' => $link['6_month'],
-                        '12_month' => $link['12_month'],
-                        '18_month' => $link['18_month'],
-                        '24_month' => $link['24_month'],
-                    ];
-                }
-
-                $tenors = ['6', '12', '18', '24'];
-                $strukturTenors = [];
-
-                foreach ($tenors as $tenor) {
-                    $tenorData = ['tenor' => (int) $tenor];
-                    foreach ($struktur as $s) {
-                        $tenorData[$s['fee_name']] = (float) $s[$tenor . '_month'];
-                    }
-                    $strukturTenors['tenor_'. $tenor] = $tenorData;
-                }
-
-                $build[] = [
-                    'tipe' => $value->category,
-                    'range_start' => (float) $value->start_value,
-                    'range_end' =>(float) $value->end_value,
-                    'tenor_6' =>$strukturTenors['tenor_6'],
-                    'tenor_12' =>$strukturTenors['tenor_12'],
-                    'tenor_18' =>$strukturTenors['tenor_18'],
-                    'tenor_24' =>$strukturTenors['tenor_24']
-                ];
-            }            
-
-            return response()->json($build, 200);
+            return response()->json($show, 200);
         } catch (\Exception $e) {
+            ActivityLogger::logActivity($request,$e->getMessage(),500);
+            return response()->json(['message' => $e->getMessage(),"status" => 500], 500);
+        }
+    }
+
+    public function show(Request $request,$id)
+    {
+        try {
+            $data = M_AdminFee::with('links')->where('id',$id)->get();
+
+            if ($data->isEmpty()) {
+                throw new Exception("Data Not Found", 404);
+            }
+
+            $show = $this->buildArray($data);
+    
+            return response()->json($show, 200);
+        } catch (Exception $e) {
             ActivityLogger::logActivity($request,$e->getMessage(),500);
             return response()->json(['message' => $e->getMessage(),"status" => 500], 500);
         }
@@ -103,4 +86,90 @@ class AdminFeeController extends Controller
             return response()->json(['message' => $e->getMessage(),"status" => 500], 500);
         }
     }
+
+    // public function update(Request $request,$id)
+    // {
+    //     DB::beginTransaction();
+    //     try {
+
+    //         $data_admin_fee =[
+    //             'category' => $request->kategory,
+    //             'start_value' => $request->start_value,
+    //             'end_value' => $request->end_value
+    //         ];
+
+    //         $admin_fee_id = M_AdminFee::create($data_admin_fee);
+
+    //         if(isset($request->struktur) && is_array($request->struktur)){
+    //             foreach ($request->struktur as $value) {
+    //                 $data_admin_type = [
+    //                     'admin_fee_id' => $admin_fee_id->id,
+    //                     'fee_name' => isset($value['key'])?$value['key']:'',
+    //                     '6_month' => $value['tenor6'],
+    //                     '12_month' => $value['tenor12'],
+    //                     '18_month' => $value['tenor18'],
+    //                     '24_month' => $value['tenor24']
+    //                 ];
+                   
+    //                 M_AdminType::create($data_admin_type);
+    //             }
+    //         }
+
+    //         DB::commit();
+    //         ActivityLogger::logActivity($request,"Success",200);
+    //         return response()->json(['message' => 'created successfully',"status" => 200], 200);
+    //     }catch (QueryException $e) {
+    //         DB::rollback();
+    //         ActivityLogger::logActivity($request,$e->getMessage(),409);
+    //         return response()->json(['message' => $e->getMessage(),"status" => 409], 409);
+    //     } catch (\Exception $e) {
+    //         DB::rollback();
+    //         ActivityLogger::logActivity($request,$e->getMessage(),500);
+    //         return response()->json(['message' => $e->getMessage(),"status" => 500], 500);
+    //     }
+    // }
+
+    public function buildArray($data){
+        $build = [];
+        
+        foreach ($data as $value) {
+          
+            $struktur = [];
+
+            foreach ($value->links as $link) {
+                $struktur[] = [
+                    'fee_name' => $link['fee_name'],
+                    '6_month' => $link['6_month'],
+                    '12_month' => $link['12_month'],
+                    '18_month' => $link['18_month'],
+                    '24_month' => $link['24_month'],
+                ];
+            }
+
+            $tenors = ['6', '12', '18', '24'];
+            $strukturTenors = [];
+
+            foreach ($tenors as $tenor) {
+                $tenorData = ['tenor' => (int) $tenor];
+                foreach ($struktur as $s) {
+                    $tenorData[$s['fee_name']] = (float) $s[$tenor . '_month'];
+                }
+                $strukturTenors['tenor_'. $tenor] = $tenorData;
+            }
+
+            $build[] = [
+                'id' => $value->id,
+                'tipe' => $value->category,
+                'range_start' => (float) $value->start_value,
+                'range_end' =>(float) $value->end_value,
+                'tenor_6' =>$strukturTenors['tenor_6'],
+                'tenor_12' =>$strukturTenors['tenor_12'],
+                'tenor_18' =>$strukturTenors['tenor_18'],
+                'tenor_24' =>$strukturTenors['tenor_24']
+            ];
+        }   
+        
+        return $build;
+    }
+    
 }
