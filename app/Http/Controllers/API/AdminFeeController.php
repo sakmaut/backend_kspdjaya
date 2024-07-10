b1f12119fe729e03ba3832d779b75594f1a52cd6
alex <alexanderlieepm@gmail.com>
2024-07-10 13:13:02 +0700

    up

--- app/Http/Controllers/API/AdminFeeController.php
+++ app/Http/Controllers/API/AdminFeeController.php
@@ -163,9 +163,9 @@ class AdminFeeController extends Controller
                 'range_start' => (float) $value->start_value,
                 'range_end' =>(float) $value->end_value,
                 ($value->category == 'bulanan' ? 'tenor_6' : 'tenor_3') =>$strukturTenors['tenor_6'],
-                'tenor_12' =>$strukturTenors['tenor_12'],
-                'tenor_18' =>$strukturTenors['tenor_18'],
-                'tenor_24' =>$strukturTenors['tenor_24']
+                ($value->category == 'bulanan' ? 'tenor_12' : 'tenor_6') =>$strukturTenors['tenor_12'],
+                ($value->category == 'bulanan' ? 'tenor_18' : 'tenor_12') =>$strukturTenors['tenor_18'],
+                ($value->category == 'bulanan' ? 'tenor_24' : 'tenor_18') =>$strukturTenors['tenor_24']
             ];
         }   
         
