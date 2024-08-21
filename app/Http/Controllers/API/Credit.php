c967fa56586703ef68d221d32674f9dfdd3851c4
alex <alexanderlieepm@gmail.com>
2024-08-21 16:23:20 +0700

    update

--- app/Http/Controllers/API/Credit.php
+++ app/Http/Controllers/API/Credit.php
@@ -77,8 +77,15 @@ class Credit extends Controller
         $data_credit_schedule = generateAmortizationSchedule($principal,$angsuran, $set_tgl_awal,$effRate, $loanTerm);
         $installment_count = count($data_credit_schedule);
 
+        print_r($data_credit_schedule);
+        die;
+
         $check_exist = M_Credit::where('ORDER_NUMBER', $request->order_number)->first();
-        // $credit_schedule = M_CreditSchedule::where('LOAN_NUMBER',$check_exist->LOAN_NUMBER)->get();
+
+        if(!$check_exist){
+            $credit_schedule = M_CreditSchedule::where('LOAN_NUMBER',$check_exist->LOAN_NUMBER)->get();
+        }
+      
         $SET_UUID = Uuid::uuid7()->toString();
 
         if (!$check_exist && $request->flag == 'yes') {
@@ -161,7 +168,7 @@ class Credit extends Controller
              "no_polisi" => $cr_guarante_vehicle->POLICE_NUMBER??null,
              "no_rangka" =>$cr_guarante_vehicle->CHASIS_NUMBER??null,
              "no_mesin" => $cr_guarante_vehicle->ENGINE_NUMBER??null,
-             "struktur" => $data_credit_schedule??null
+             "struktur" => !$credit_schedule?$credit_schedule : $data_credit_schedule??null
         ];
 
         return $data;
