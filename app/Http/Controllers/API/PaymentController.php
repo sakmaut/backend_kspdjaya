e32ea58467af5084a2d6eb6933cfb4a64d84eccc
alex <alexanderlieepm@gmail.com>
2024-09-11 10:35:57 +0700

    up

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\R_Branch;
 use App\Http\Resources\R_BranchDetail;
+use App\Models\M_Arrears;
 use App\Models\M_Branch;
 use App\Models\M_Credit;
 use App\Models\M_CreditSchedule;
@@ -26,89 +27,127 @@ class PaymentController extends Controller
     {
         DB::beginTransaction();
         try {   
-
-            $schedule = M_CreditSchedule::where('loan_number',$request->loan_number)->get();
-            $paymentAmount = $request->nilai_pembayaran;
             $getCodeBranch = M_Branch::find($request->user()->branch_id);
             $created_now = Carbon::now();
             $no_inv = generateCode($request, 'payment', 'INVOICE','INV');
+            $paymentAmount = $request->nilai_pembayaran;
 
-            $credit = M_Credit::where('LOAN_NUMBER',$request->loan_number)->first();
-            $detail_customer = M_Customer::where('CUST_CODE',$credit->CUST_CODE)->first();
+            // $pembayaran = [];
+            // foreach ($schedule as $scheduleItem) {
+            //     if ($paymentAmount > 0) {
+            //         $installment = $scheduleItem->INSTALLMENT;
+            //         $remainingPayment = $installment - $scheduleItem->PAYMENT_VALUE;
+            
+            //         if ($remainingPayment > 0) {
+            //             $paymentValue = min($paymentAmount, $remainingPayment);
+            //             $paymentAmount -= $paymentValue;
+            //             $scheduleItem->PAYMENT_VALUE += $paymentValue;
+            
+            //             if ($scheduleItem->PAYMENT_VALUE == $installment) {
+            //                 $scheduleItem->PAID_FLAG = 'PAID';
+            //             }
+            //         } else {
+            //             continue;
+            //         }
 
-            $pembayaran = [];
-            foreach ($schedule as $scheduleItem) {
-                if ($paymentAmount > 0) {
-                    $installment = $scheduleItem->INSTALLMENT;
-                    $remainingPayment = $installment - $scheduleItem->PAYMENT_VALUE;
-            
-                    if ($remainingPayment > 0) {
-                        $paymentValue = min($paymentAmount, $remainingPayment);
-                        $paymentAmount -= $paymentValue;
-                        $scheduleItem->PAYMENT_VALUE += $paymentValue;
-            
-                        if ($scheduleItem->PAYMENT_VALUE == $installment) {
-                            $scheduleItem->PAID_FLAG = 'PAID';
-                        }
-                    } else {
-                        continue;
+            //         $pembayaran[] = [
+            //             'installment' => $scheduleItem->INSTALLMENT_COUNT,
+            //             'payment_value' => $paymentValue,
+            //             'original_amount' => $request->nilai_pembayaran,
+            //             'title' => 'Angsuran Ke-'.$scheduleItem->INSTALLMENT_COUNT,
+            //         ];
+
+            //         $arrayData = [  
+            //             'ID' => Uuid::uuid7()->toString(),
+            //             'STTS_RCRD' => 'A',
+            //             'INVOICE' => $no_inv,
+            //             'NO_TRX' => generateCode($request, 'payment', 'NO_TRX','TRX'),
+            //             'PAYMENT_METHOD' => $request->payment_method,
+            //             'BRANCH' => $getCodeBranch->CODE_NUMBER,
+            //             'LOAN_NUM' => $request->loan_number,
+            //             'VALUE_DATE' => $request->tgl_bayar,
+            //             'ENTRY_DATE' => $created_now,
+            //             'TITLE' => 'Angsuran Ke-'.$scheduleItem->INSTALLMENT_COUNT,
+            //             'ORIGINAL_AMOUNT' => $request->nilai_pembayaran,
+            //             'OS_AMOUNT' => 0,
+            //             'AUTH_BY' => $request->user()->id,
+            //             'AUTH_DATE' => $created_now,
+            //             'BANK_NAME' => $request->nama_bank??null,
+            //             'BANK_ACC_NUMBER' => $request->no_rekening??null,
+            //         ];
+
+            //         // if($paymentAmount != 0){
+            //         //     M_Payment::create($arrayData);
+            //         // }
+                   
+            //         // $scheduleItem->save();
+            //     } else {
+            //         break;
+            //     }
+            // }
+
+            $customer_detail=[];
+            if (isset($request->struktur) && is_array($request->struktur)) {
+               foreach ($request->struktur as $res) {
+
+                    $loan_number = $res['loan_number'];
+                    $tgl_agsuran =  Carbon::parse($res['tgl_angsuran'])->format('Y-m-d');
+
+                    $credit = M_Credit::where('LOAN_NUMBER',$loan_number)->first();
+                    $detail_customer = M_Customer::where('CUST_CODE',$credit->CUST_CODE)->first();
+
+                    $check_arrears = M_Arrears::where(['LOAN_NUMBER' => $loan_number,'START_DATE' => $tgl_agsuran])->first();
+                    if($check_arrears){
+                        $check_arrears->update([
+                            'PAID_PENALTY' => $res['bayar_denda']
+                        ]);
                     }
 
-                    $pembayaran[] = [
-                        'installment' => $scheduleItem->INSTALLMENT_COUNT,
-                        'payment_value' => $paymentValue,
-                        'original_amount' => $request->nilai_pembayaran,
-                        'title' => 'Angsuran Ke-'.$scheduleItem->INSTALLMENT_COUNT,
+                    $customer_detail = [
+                        'cust_code' =>  $detail_customer->CUST_CODE,
+                        'nama' => $detail_customer->NAME,
+                        'alamat' => $detail_customer->ADDRESS,
+                        'rt' => $detail_customer->RT,
+                        'rw' => $detail_customer->RW,
+                        'provinsi' => $detail_customer->PROVINCE,
+                        'kota' => $detail_customer->CITY,
+                        'kelurahan' => $detail_customer->KELURAHAN,
+                        'kecamatan' => $detail_customer->KECAMATAN,
                     ];
 
-                    $arrayData = [  
+                    $data = [  
                         'ID' => Uuid::uuid7()->toString(),
                         'STTS_RCRD' => 'A',
                         'INVOICE' => $no_inv,
                         'NO_TRX' => generateCode($request, 'payment', 'NO_TRX','TRX'),
                         'PAYMENT_METHOD' => $request->payment_method,
                         'BRANCH' => $getCodeBranch->CODE_NUMBER,
-                        'LOAN_NUM' => $request->loan_number,
-                        'VALUE_DATE' => $request->tgl_bayar,
+                        'LOAN_NUM' => $loan_number,
+                        'VALUE_DATE' => null,
                         'ENTRY_DATE' => $created_now,
-                        'TITLE' => 'Angsuran Ke-'.$scheduleItem->INSTALLMENT_COUNT,
-                        'ORIGINAL_AMOUNT' => $request->nilai_pembayaran,
+                        'TITLE' => 'Angsuran Ke-'.$res['angsuran_ke'],
+                        'ORIGINAL_AMOUNT' => $res['bayar_angsuran']+ $res['bayar_denda'],
                         'OS_AMOUNT' => 0,
+                        'START_DATE' => $tgl_agsuran,
                         'AUTH_BY' => $request->user()->id,
                         'AUTH_DATE' => $created_now,
                         'BANK_NAME' => $request->nama_bank??null,
                         'BANK_ACC_NUMBER' => $request->no_rekening??null,
                     ];
 
-                    // if($paymentAmount != 0){
-                    //     M_Payment::create($arrayData);
-                    // }
-                   
-                    // $scheduleItem->save();
-                } else {
-                    break;
-                }
+                    M_Payment::create($data);
+               }
             }
 
             $build = [
                 "no_transaksi" => $no_inv,
-                "detail_pelanggan" => [
-                    'cust_code' =>  $detail_customer->CUST_CODE,
-                    'nama' => $detail_customer->NAME,
-                    'alamat' => $detail_customer->ADDRESS,
-                    'rt' => $detail_customer->RT,
-                    'rw' => $detail_customer->RW,
-                    'provinsi' => $detail_customer->PROVINCE,
-                    'kota' => $detail_customer->CITY,
-                    'kelurahan' => $detail_customer->KELURAHAN,
-                    'kecamatan' => $detail_customer->KECAMATAN,
-                ],
+                "detail_pelanggan" => $customer_detail,
                 "tgl_transaksi" => Carbon::now()->format('d-m-Y'),
                 "payment_method" => $request->payment_method,
                 "nama_bank" => $request->nama_bank,
                 "no_rekening" => $request->no_rekening,
                 "bukti_transfer" => '',
-                "pembayaran" => $pembayaran,
+                // "pembayaran" => $pembayaran,
                 "pembulatan" => $request->pembulatan,
                 "kembalian" => $request->kembalian,
                 "jml_pembayaran" => $request->nilai_pembayaran,
