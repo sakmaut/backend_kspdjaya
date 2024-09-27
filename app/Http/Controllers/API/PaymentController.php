9f5f60637c61750ae5d7fdcb8537253b1a8e36dd
alex <alexanderlieepm@gmail.com>
2024-09-27 09:28:27 +0700

    sss

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -81,7 +81,7 @@ class PaymentController extends Controller
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
-
+    //upd
     public function paymentPelunasan(Request $request)
     {
         DB::beginTransaction();
