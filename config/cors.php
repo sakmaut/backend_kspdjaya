d55ea796020bc5300d160e75c919997ff97126f3
alex <alexanderlieepm@gmail.com>
2024-10-10 16:04:17 +0700

    up

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://los.kspdjaya.id','https://dapur.kspdjaya.id'],
+    'allowed_origins' => ['http://localhost:5173','http://192.168.1.17:5173', 'https://los.kspdjaya.id','https://dapur.kspdjaya.id'],
 
     'allowed_origins_patterns' => [],
 
