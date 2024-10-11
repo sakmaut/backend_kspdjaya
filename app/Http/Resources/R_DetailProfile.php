40f73e951c247eb9423584bf81cf6b291cc6b975
alex <alexanderlieepm@gmail.com>
2024-10-11 10:14:05 +0700

    up

--- app/Http/Resources/R_DetailProfile.php
+++ app/Http/Resources/R_DetailProfile.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\M_Branch;
 use App\Models\M_HrEmployeeDocument;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -17,15 +18,22 @@ class R_DetailProfile extends JsonResource
     {
         $attachment = M_HrEmployeeDocument::where('USERS_ID',$request->user()->id)->get();
 
+        $branch = M_Branch::find($request->user()->id);
+        
         return [
-            'USER_ID' => $request->user()->id,
-            'USERNAME' => $request->user()->username,
-            'EMAIL' => $request->user()->email,
-            'NAMA' => $request->user()->fullname,
-            "GENDER" => $request->user()->gender,
-            "HP" => $request->user()->mobile_number,
-            "STATUS" => $request->user()->status,
-            "ATTACHMENT" => [$attachment]
+            'id' => $request->user()->id,
+            'username' => $request->user()->username,
+            'nama' => $request->user()->fullname,
+            'email' => $request->user()->email,
+            'cabang_id' => $branch->ID ?? null,
+            'cabang_nama' => $branch->NAME ?? null,
+            'jabatan' =>$request->user()->position,
+            'no_ktp' => $request->user()->no_ktp,
+            'alamat' =>$request->user()->alamat,
+            'gender' => $request->user()->gender,
+            'no_hp' => $request->user()->mobile_number,
+            'status' => $request->user()->status,
+            'attachment' => [$attachment]
         ];
     }
 }
