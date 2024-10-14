813fb1dd6102de4bdab52e70116fe41da854a084
alex <alexanderlieepm@gmail.com>
2024-10-14 16:53:38 +0700

    ss

--- app/Models/M_CrSurveyDocument.php
+++ app/Models/M_CrSurveyDocument.php
@@ -34,7 +34,8 @@ class M_CrSurveyDocument extends Model
     }
 
     public static function attachment($survey_id, $array = []) {
-        $attachment = self::where('CR_SURVEY_ID', $survey_id)
+        $attachment = self::on('cr_survey_document')
+                        ->where('CR_SURVEY_ID', $survey_id)
                         ->whereIn('TYPE', $array)
                         ->groupBy('type')
                         ->orderBy('CREATED_AT', 'desc')
