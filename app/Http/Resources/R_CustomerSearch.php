a9c699b1152bc328410ed324c82377ed3d217a88
alex <alexanderlieepm@gmail.com>
2024-11-18 14:56:59 +0700

    up

--- app/Http/Resources/R_CustomerSearch.php
+++ app/Http/Resources/R_CustomerSearch.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\M_CreditSchedule;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,13 +15,21 @@ class R_CustomerSearch extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
+        $data = M_CreditSchedule::where('LOAN_NUMBER', $this->LOAN_NUMBER)
+        ->where(function ($query) {
+            $query->whereNull('PAID_FLAG')
+                ->orWhere('PAID_FLAG', '<>', 'PAID');
+        })->get();
+
         return [
             "loan_number" => $this->LOAN_NUMBER,
             "no_kontrak" => $this->ORDER_NUMBER,
             "nama" => $this->NAME,
             "no_polisi" => $this->POLICE_NUMBER,
             "alamat" => $this->ADDRESS,
-            "angsuran" => intval($this->INSTALLMENT)
+            "angsuran" => intval($this->INSTALLMENT),
+            'status' => $data->isEmpty() ? 'LUNAS':'BELUM LUNAS'
         ];
     }
 }
