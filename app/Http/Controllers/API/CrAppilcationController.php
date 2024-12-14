01869b2b6c4c596168101b678829df89e883a57d
alex <alexanderlieepm@gmail.com>
2024-12-14 14:25:13 +0700

    up

--- app/Http/Controllers/API/CrAppilcationController.php
+++ app/Http/Controllers/API/CrAppilcationController.php
@@ -1051,7 +1051,7 @@ class CrAppilcationController extends Controller
             ],
             "info_bank" =>[],
             "ekstra" =>[
-                'jenis_angsuran' => empty($application->INSTALLMENT_TYPE)?$cr_survey->jenis_angsuran:$application->INSTALLMENT_TYPE,
+                'jenis_angsuran' => strtolower(empty($application->INSTALLMENT_TYPE)?$cr_survey->jenis_angsuran:$application->INSTALLMENT_TYPE),
                 'tenor' => $application->TENOR,
                 "nilai_yang_diterima" => $applicationDetail->SUBMISSION_VALUE == ''?(int) $data->plafond:(int)$applicationDetail->SUBMISSION_VALUE?? null,
                 "total"=> (int)$applicationDetail->TOTAL_ADMIN?? null,
