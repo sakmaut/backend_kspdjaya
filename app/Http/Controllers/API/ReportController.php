c52fd7bd5fd6e9518571563ebb48ca28c0825fa0
alex <alexanderlieepm@gmail.com>
2024-12-24 14:47:09 +0700

    up

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\M_Arrears;
+use App\Models\M_Branch;
 use App\Models\M_CrCollateral;
 use App\Models\M_CrCollateralSertification;
 use App\Models\M_Credit;
 use App\Models\M_Payment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -59,7 +61,30 @@ class ReportController extends Controller
         try {
             $results = M_Credit::where('ID',$id)->first();
 
-            return response()->json($results, 200);
+            if(!$results){
+                $buildArray = [];
+            }else{
+                $buildArray =[
+                    'status' => $results->STATUS,
+                    'loan_number' => $results->LOAN_NUMBER,
+                    'cust_code' => $results->CUST_CODE,
+                    'branch_name' => M_Branch::find($results->BRANCH)->NAME??'',
+                    'order_number' => $results->ORDER_NUMBER,
+                    'credit_type' => $results->CREDIT_TYPE,
+                    'tenor' => (int)$results->PERIOD,
+                    'installment_date' => date('Y-m-d',strtotime($results->INSTALLMENT_DATE)),
+                    'installment' => floatval($results->INSTALLMENT),
+                    'pcpl_ori' => floatval($results->PCPL_ORI),
+                    'paid_principal' => floatval($results->PAID_PRINCIPAL),
+                    'paid_interest' => floatval($results->PAID_INTEREST),
+                    'paid_penalty' => floatval($results->PAID_PENALTY),
+                    'mcf_name' => User::find($results->MCF_ID)->fullname??'',
+                    'created_by' => User::find($results->CREATED_BY)->fullname??'',
+                    'created_at' => date('Y-m-d',strtotime($results->CREATED_AT))
+                ];
+            }
+
+            return response()->json($buildArray, 200);
         } catch (\Exception $e) {
             ActivityLogger::logActivity($request,$e->getMessage(),500);
             return response()->json(['message' => $e->getMessage(),"status" => 500], 500);
