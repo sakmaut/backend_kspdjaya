06966f7e7d45c65ae04729040b58b090fb596fd5
alex <alexanderlieepm@gmail.com>
2025-01-09 15:12:47 +0700

    up

--- app/Http/Resources/R_CreditCancelLog.php
+++ app/Http/Resources/R_CreditCancelLog.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use App\Models\M_BpkbDetail;
 use App\Models\M_Branch;
+use App\Models\M_Credit;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -19,9 +20,13 @@ class R_CreditCancelLog extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
+        $credit = M_Credit::where('ORDER_NUMBER',$this->ORDER_NUMBER)->first();
+
         return [
             "id" => $this->ID,
-            "credit_id" =>$this->CREDIT_ID ??null,
+            "no_kontrak" =>$credit->LOAN_NUMBER ??null,
+            "tgl_kontrak" =>$credit->ENTRY_DATE ??null,
             "request_by" => User::find($this->REQUEST_BY)->fullname ??null,
             "request_branch" => M_Branch::find($this->REQUEST_BRANCH)->NAME??null,
             "request_date" => $this->REQUEST_DATE??null,
