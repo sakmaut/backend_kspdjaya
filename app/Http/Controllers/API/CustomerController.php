c80a792b1537c2b6ab7c2ccb567766bd40274db3
alex <alexanderlieepm@gmail.com>
2025-01-25 14:19:43 +0700

    up

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -134,7 +134,8 @@ class CustomerController extends Controller
                             ->leftJoin('arrears as b', function($join) {
                                 $join->on('b.LOAN_NUMBER', '=', 'a.LOAN_NUMBER')
                                     ->whereRaw('b.START_DATE = a.PAYMENT_DATE')
-                                    ->where('b.STATUS_REC', '=', 'A');
+                                    ->where('b.STATUS_REC', '=', 'A')
+                                    ->where('b.PAST_DUE_PENALTY', '!=', 0);
                             })
                             ->where('a.LOAN_NUMBER', $loanNumber)
                             ->select('a.*', 'b.ID as id_arrear', 'b.PAST_DUE_PENALTY', 'b.PAID_PENALTY')
