c7bfd4b69846b70bd15a4164062f2308f2a1af30
alex <alexanderlieepm@gmail.com>
2025-01-30 17:21:33 +0700

    u

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,8 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('demo:cron')->dailyAt('16:52');
-        // $schedule->command('demo:cron')->dailyAt('01:30');
+        // $schedule->command('demo:cron')->dailyAt('16:52');
+        $schedule->command('demo:cron')->dailyAt('00:30');
     }
 
     /**
