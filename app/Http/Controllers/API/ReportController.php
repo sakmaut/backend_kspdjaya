4c3e40ebc35bfbfa5dcce43ec1d4121a49369f10
alex <alexanderlieepm@gmail.com>
2025-02-10 12:17:47 +0700

    ih

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -518,11 +518,11 @@ class ReportController extends Controller
                             left join branch e on e.ID = a.LOCATION_BRANCH
                             left join bpkb_detail f on f.COLLATERAL_ID = a.ID
                     WHERE	(1=1)
-                                and d.NAME = '$request->pos'
-                                and b.LOAN_NUMBER like '%$request->loan_number%'
-                                and c.NAME like '%$request->nama%'
-                                and a.POLICE_NUMBER like '%$request->nopol%'
-                                and coalesce(f.STATUS,'NORMAL') = '$request->status'
+                                and d.NAME = '$request->pos??'
+                                and b.LOAN_NUMBER like '%$request->loan_number??%'
+                                and c.NAME like '%$request->nama??%'
+                                and a.POLICE_NUMBER like '%$request->nopol??%'
+                                and coalesce(f.STATUS,'NORMAL') = '$request->status??'
                     ORDER	BY d.NAME, e.NAME, b.LOAN_NUMBER, c.NAME,
                             a.POLICE_NUMBER, f.STATUS";
 
