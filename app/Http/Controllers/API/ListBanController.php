dea896c166fc3d37671e38b7622e7515fc2cd9bd
alex <alexanderlieepm@gmail.com>
2025-02-12 14:09:02 +0700

    up

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -20,9 +20,7 @@ class ListBanController extends Controller
 
             if (!empty($request->dari)) {
 
-                $timestamp = $request->dari / 1000;
-
-                // Convert the timestamp to a Carbon instance
+                $timestamp = intval($request->dari) / 1000;
                 $date = Carbon::createFromTimestamp($timestamp);
 
                 // Format the date as Y-m-d
@@ -37,22 +35,42 @@ class ListBanController extends Controller
                 $totalCashin = 0;
                 $totalAmount = 0;
 
+                $cash_in = [];
+
                 foreach ($arusKas as $item) {
-                    // Handle 'CASH-IN'
+
+                    $row = $item->no_invoice . $item->LOAN_NUM . $item->PELANGGAN;
+
+                    $no_invoice = $item->no_invoice;
+                    $loan_num = $item->LOAN_NUM;
+                    $pelanggan = $item->PELANGGAN;
+
+                    if (in_array($row, $cash_in)) {
+                        $no = 0;
+                        $no_invoice = '';
+                        $loan_num = '';
+                        $pelanggan = '';
+                    } else {
+                        array_push($cash_in, $row);
+                        $no++;
+                    }
+
+                    $amount = is_numeric($item->ORIGINAL_AMOUNT) ? floatval($item->ORIGINAL_AMOUNT) : 0;
+
                     if ($item->JENIS != 'PENCAIRAN') {
                         $datas['datas'][] = [
-                            'no' => $no++,
+                            'no' => $no == 0 ? '' : $no,
                             'type' => 'CASH_IN',
-                            'no_invoice' => $item->no_invoice ?? '',
-                            'no_kontrak' => $item->LOAN_NUM ?? '',
+                            'no_invoice' => $no_invoice,
+                            'no_kontrak' => $loan_num,
                             'tgl' => $item->ENTRY_DATE ?? '',
                             'cabang' => $item->nama_cabang ?? '',
                             'user' => $item->fullname ?? '',
                             'position' => $item->position ?? '',
-                            'nama_pelanggan' => $item->PELANGGAN ?? '',
+                            'nama_pelanggan' => $pelanggan,
                             'metode_pembayaran' => $item->PAYMENT_METHOD ?? '',
                             'keterangan' => $item->JENIS . ' ' . $item->angsuran_ke ?? '',
-                            'amount' => floatval($item->ORIGINAL_AMOUNT),
+                            'amount' => $amount,
                         ];
 
                         $totalCashin += floatval($item->ORIGINAL_AMOUNT);
@@ -169,172 +187,6 @@ class ListBanController extends Controller
 
         return $result;
     }
-
-    // public function listBan(Request $request) {
-    //     try {
-
-    //         $dateFrom = $request->dari;
-    //         $getBranch = $request->cabang_id;
-
-    //         $results = DB::table('branch as a')
-    //                     ->join('credit as b', 'b.BRANCH', '=', 'a.ID')
-    //                     ->leftJoin('customer as c', 'c.CUST_CODE', '=', 'b.CUST_CODE')
-    //                     ->leftJoin('users as d', 'd.id', '=', 'b.MCF_ID')
-    //                     ->leftJoin('cr_application as e', 'e.ORDER_NUMBER', '=', 'b.ORDER_NUMBER')
-    //                     ->leftJoin('cr_order as h', 'h.APPLICATION_ID', '=', 'e.ID')
-    //                     ->leftJoin('cr_survey as f', 'f.id', '=', 'e.CR_SURVEY_ID')
-    //                     ->leftJoin('cr_collateral as g', 'g.CR_CREDIT_ID', '=', 'b.ID')
-    //                     ->select(
-    //                         DB::raw("CONCAT(a.CODE, '-', a.CODE_NUMBER) as KODE"),
-    //                         'a.NAME as NAMA_CABANG',
-    //                         'b.LOAN_NUMBER as NO_KONTRAK',
-    //                         'c.NAME as NAMA_PELANGGAN',
-    //                         'b.CREATED_AT as TGL_BOOKING',
-    //                         DB::raw('null as UB'),
-    //                         DB::raw('null as PLATFORM'),
-    //                         'c.INS_ADDRESS as ALAMAT_TAGIH',
-    //                         'c.ZIP_CODE as KODE_POST',
-    //                         'c.PHONE_HOUSE as NO_TELP',
-    //                         'c.PHONE_PERSONAL as NO_HP',
-    //                         'c.OCCUPATION as PEKERJAAN',
-    //                         DB::raw('null as SURVEYOR'),
-    //                         'f.survey_note as CATT_SURVEY',
-    //                         'b.PCPL_ORI as PKK_HUTANG',
-    //                         'e.INSTALLMENT_TYPE as tipe',
-    //                         'b.PERIOD',
-    //                         DB::raw('DATEDIFF(b.FIRST_ARR_DATE, NOW()) as OVERDUE'),
-    //                         DB::raw('99 as CYCLE'),
-    //                         'b.STATUS_REC',
-    //                         'b.PAID_PRINCIPAL',
-    //                         'b.PAID_INTEREST',
-    //                         DB::raw('b.PAID_PRINCIPAL + b.PAID_INTEREST as PAID_TOTAL'),
-    //                         DB::raw('b.PCPL_ORI - b.PAID_PRINCIPAL as OUTSTANDING'),
-    //                         'b.INSTALLMENT',
-    //                         'b.INSTALLMENT_DATE',
-    //                         'b.FIRST_ARR_DATE',
-    //                         DB::raw("' ' as COLLECTOR"),
-    //                         DB::raw('GROUP_CONCAT(CONCAT(g.BRAND, " ", g.TYPE)) as COLLATERAL'),
-    //                         DB::raw('GROUP_CONCAT(g.POLICE_NUMBER) as POLICE_NUMBER'),
-    //                         DB::raw('GROUP_CONCAT(g.ENGINE_NUMBER) as ENGINE_NUMBER'),
-    //                         DB::raw('GROUP_CONCAT(g.CHASIS_NUMBER) as CHASIS_NUMBER'),
-    //                         DB::raw('GROUP_CONCAT(g.PRODUCTION_YEAR) as PRODUCTION_YEAR'),
-    //                         DB::raw('SUM(g.VALUE) as TOTAL_NILAI_JAMINAN'),
-    //                         'b.CUST_CODE',
-    //                         DB::raw("concat(h.REF_PELANGGAN,' ',h.REF_PELANGGAN_OTHER) as supplier")
-    //                     )
-    //                     ->groupBy(
-    //                         'a.CODE',
-    //                         'a.CODE_NUMBER',
-    //                         'a.NAME',
-    //                         'b.LOAN_NUMBER',
-    //                         'c.NAME',
-    //                         'b.CREATED_AT',
-    //                         'c.INS_ADDRESS',
-    //                         'c.ZIP_CODE',
-    //                         'c.PHONE_HOUSE',
-    //                         'c.PHONE_PERSONAL',
-    //                         'c.OCCUPATION',
-    //                         'd.fullname',
-    //                         'f.survey_note',
-    //                         'b.PCPL_ORI',
-    //                         'e.TOTAL_ADMIN',
-    //                         'e.INSTALLMENT_TYPE',
-    //                         'b.PERIOD',
-    //                         DB::raw('DATEDIFF(b.FIRST_ARR_DATE, NOW())'),
-    //                         'b.STATUS_REC',
-    //                         'b.PAID_PRINCIPAL',
-    //                         'b.PAID_INTEREST',
-    //                         DB::raw('b.PAID_PRINCIPAL + b.PAID_INTEREST'),
-    //                         DB::raw('b.PCPL_ORI - b.PAID_PRINCIPAL'),
-    //                         'b.INSTALLMENT',
-    //                         'b.INSTALLMENT_DATE',
-    //                         'b.FIRST_ARR_DATE',
-    //                         'b.CUST_CODE',
-    //                         'h.REF_PELANGGAN',
-    //                         'h.REF_PELANGGAN_OTHER'
-    //                     );
-
-    //         if (!empty($getBranch) && $getBranch != 'SEMUA CABANG') {
-    //             $results->where('a.ID', $getBranch);
-    //         }
-
-    //         if (!empty($dateFrom)) {
-    //             $results->where(DB::raw("DATE_FORMAT(b.CREATED_AT, '%Y-%m')"), $dateFrom);
-    //         } else {
-    //             $results->where('b.CREATED_AT', '>=', DB::raw('DATE_SUB(NOW(), INTERVAL 1 MONTH)'));
-    //         }
-
-    //         $results = $results->get();
-
-    //         $build = [];
-    //         foreach ($results as $result) {
-    //             $build[] =[
-    //                 "KODE CABANG" => $result->KODE ?? '',
-    //                 "NAMA CABANG" => $result->NAMA_CABANG ?? '',
-    //                 "NO KONTRAK" => $result->NO_KONTRAK ?? '',
-    //                 "NAMA PELANGGAN" => $result->NAMA_PELANGGAN ?? '',
-    //                 "TGL BOOKING" => isset($result->TGL_BOOKING) && !empty($result->TGL_BOOKING) ? date("d-m-Y", strtotime($result->TGL_BOOKING)) : '',
-    //                 "UB" => $result->UB ?? '',
-    //                 "PLATFORM" => $result->PLATFORM ?? '',
-    //                 "ALAMAT TAGIH" => $result->ALAMAT_TAGIH ?? '',
-    //                 "KODEPOS" => $result->KODE_POST ?? '',
-    //                 "SUBZIP" => '',
-    //                 "NO TELP" => $result->NO_TELP ?? '',
-    //                 "NO HP1" => $result->NO_HP ?? '',
-    //                 "NO HP2" => $result->NO_HP ?? '',
-    //                 "PEKERJAAN" => $result->PEKERJAAN ?? '',
-    //                 "SUPPLIER" => $result->supplier??'',
-    //                 "SURVEYOR" => $result->SURVEYOR ?? '',
-    //                 "CATT SURVEY" => $result->CATT_SURVEY ?? '',
-    //                 "PKK HUTANG" => number_format($result->PKK_HUTANG ?? 0),
-    //                 "JML ANGS" => $result->PERIOD ?? '',
-    //                 "JRK ANGS" => $result->PERIOD ?? '',
-    //                 "PERIOD" => $result->tipe ?? '',
-    //                 "OUT PKK AWAL" => '',
-    //                 "OUT BNG AWAL" => '',
-    //                 "OVERDUE AWAL" => number_format($result->OVERDUE ?? 0),
-    //                 "AMBC PKK AWAL" => '',
-    //                 "AMBC BNG AWAL" => '',
-    //                 "AMBC TOTAL AWAL" => '',
-    //                 "CYCLE AWAL" => $result->CYCLE ?? '',
-    //                 "STS KONTRAK" => $result->STATUS_REC ?? '',
-    //                 "STS BEBAN" => '',
-    //                 "POLA BYR AWAL" => '',
-    //                 "OUTS PKK AKHIR" => number_format($result->PAID_PRINCIPAL ?? 0),
-    //                 "OUTS BNG AKHIR" => number_format($result->PAID_INTEREST ?? 0),
-    //                 "OVERDUE AKHIR" => number_format($result->OUTSTANDING ?? 0),
-    //                 "ANGSURAN" => number_format($result->INSTALLMENT ?? 0),
-    //                 "ANGS KE" => '',
-    //                 "TIPE ANGSURAN"=>'',
-    //                 "JTH TEMPO AWAL" => date("d-m-Y", strtotime($result->INSTALLMENT_DATE ?? '')),
-    //                 "JTH TEMPO AKHIR" => date("d-m-Y", strtotime($result->FIRST_ARR_DATE ?? '')),
-    //                 "TGL BAYAR" => '',
-    //                 "KOLEKTOR" => '',
-    //                 "CARA BYR" => '',
-    //                 "AMBC PKK_AKHIR" => '',
-    //                 "AMBC BNG_AKHIR" => '',
-    //                 "AMBC TOTAL_AKHIR" => '',
-    //                 "AC PKK"=>'',
-    //                 "AC BNG MRG"=>'',
-    //                 "AC TOTAL"=>'',
-    //                 "CYCLE AKHIR"=>'',
-    //                 "POLA BYR AKHIR"=>'',
-    //                 "NAMA BRG"=>'',
-    //                 "TIPE BRG" =>  $result->COLLATERAL ?? '',
-    //                 "NO POL" =>  $result->POLICE_NUMBER ?? '',
-    //                 "NO MESIN" =>  $result->ENGINE_NUMBER ?? '',
-    //                 "NO RANGKA" =>  $result->CHASIS_NUMBER ?? '',
-    //                 "TAHUN" =>  $result->PRODUCTION_YEAR ?? '',
-    //                 "NILAI PINJAMAN" => number_format($result->TOTAL_NILAI_JAMINAN ?? 0),
-    //                 "ADMIN" =>  $result->TOTAL_ADMIN ?? '',
-    //                 "CUST_ID" =>  $result->CUST_CODE??'',
-    //             ] ;
-    //         }
-    //         return response()->json($build, 200);           
-    //     }catch (\Exception $e) {
-    //         return response()->json(['message' => $e->getMessage(), "status" => 500], 500);
-    //     }
-    // }
 
     public function listBan(Request $request)
     {
