35d7b733b6dc05f9864d614fbf924f5fee7462cf
alex <alexanderlieepm@gmail.com>
2025-02-13 11:37:36 +0700

    dd

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -574,7 +574,7 @@ class ReportController extends Controller
                     'Sisa Angs' => number_format($sisaAngs),
                     'Denda' => number_format($res->PAST_DUE_PENALTY ?? 0),
                     'Byr Dnda' => number_format($res->denda ?? 0),
-                    'Sisa Ttl Tghn' => $sisaByr,
+                    'Sisa Tghn' => $sisaByr,
                     'Ovd' => $res->OD ?? 0,
                     '' => $sisaByr == '0' ? 'L' : ''
                 ];
