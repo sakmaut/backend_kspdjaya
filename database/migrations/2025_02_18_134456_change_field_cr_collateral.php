a6b975decfe3815d25a2446baac4fedf142ed75d
alex <alexanderlieepm@gmail.com>
2025-02-18 13:48:44 +0700

    up

---
+++ database/migrations/2025_02_18_134456_change_field_cr_collateral.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cr_collateral', function (Blueprint $table) {
+            $table->string('BPKB_ADDRESS')->nullable()->change();
+            $table->string('INVOICE_NUMBER')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
