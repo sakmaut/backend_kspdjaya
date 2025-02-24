13f32bb9dddecda6140b33cfcb803335567fee48
alex <alexanderlieepm@gmail.com>
2025-02-24 22:31:05 +0700

    ss

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -481,7 +481,7 @@ class ReportController extends Controller
                                 WHEN c.PAST_DUE_PENALTY != 0 THEN 
                                     GREATEST(
                                         DATEDIFF(
-                                            COALESCE(mp.ENTRY_DATE,NOW()),
+                                            COALESCE(mp.ENTRY_DATE,DATE_FORMAT(NOW(),%Y-%m-%d)),
                                             -- CASE
                                             --     WHEN mp.ENTRY_DATE IS NULL OR TRIM(mp.ENTRY_DATE) = '' THEN NOW()
                                             --     ELSE mp.ENTRY_DATE
