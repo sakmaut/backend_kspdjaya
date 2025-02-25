bdb0af4df840a23827589c5452f7e44c496edce2
alex <alexanderlieepm@gmail.com>
2025-02-25 08:05:35 +0700

    ok

--- app/Console/Commands/DemoCron.php
+++ app/Console/Commands/DemoCron.php
@@ -34,6 +34,10 @@ class DemoCron extends Command
      */
     public function handle()
     {
+
+
+        DB::beginTransaction();
+
         try {
 
             $setDate = DB::raw('CURDATE()');
@@ -52,13 +56,15 @@ class DemoCron extends Command
                     'STATUS' => 'SUCCESS',
                     'DESCRIPTION' => 'DATA ARREARS EMPTY'
                 ]);
+                DB::commit();
                 return;
             }
 
             $arrearsData = [];
+            $today = date('Y-m-d');
+
             foreach ($query as $result) {
-                $date = date('Y-m-d');
-                $daysDiff = (strtotime($date) - strtotime($result->PAYMENT_DATE)) / (60 * 60 * 24);
+                $daysDiff = (strtotime($today) - strtotime($result->PAYMENT_DATE)) / (60 * 60 * 24);
                 $pastDuePenalty = $result->INSTALLMENT * ($daysDiff * 0.005);
 
                 $arrearsData[] = [
@@ -77,6 +83,7 @@ class DemoCron extends Command
                 ];
             }
 
+            // Batch insert or update
             foreach ($arrearsData as $data) {
                 $existingArrears = M_Arrears::where([
                     'LOAN_NUMBER' => $data['LOAN_NUMBER'],
@@ -85,42 +92,111 @@ class DemoCron extends Command
                 ])->first();
 
                 if ($existingArrears) {
-                    // Update the existing record
                     $existingArrears->update([
                         'PAST_DUE_PENALTY' => $data['PAST_DUE_PENALTY'] ?? 0,
                         'UPDATED_AT' => Carbon::now('Asia/Jakarta')
                     ]);
                 } else {
-                    // Insert new record
                     M_Arrears::create($data);
                 }
             }
 
-            DB::table('first_arr')->delete();
-
-            $data = DB::table('arrears')
-                ->selectRaw('LOAN_NUMBER, min(START_DATE) as start_date, datediff(now(), min(START_DATE)) as date_diff')
-                ->where('status_rec', 'A')
-                ->groupBy('LOAN_NUMBER')
-                ->get();
-
-            foreach ($data as $row) {
-                M_FirstArr::create([
-                    'LOAN_NUMBER' => $row->LOAN_NUMBER,
-                    'START_DATE' => $row->start_date,
-                    'DATE_DIFF' => $row->date_diff
-                ]);
-            }
-
-            M_CronJobLog::create([
-                'STATUS' => 'SUCCESS',
-                'DESCRIPTION' => 'Records processed successfully'
-            ]);
+            DB::commit();
         } catch (\Exception $e) {
+            DB::rollBack();
             M_CronJobLog::create([
-                'STATUS' => 'FAIL',
+                'STATUS' => 'FAILED',
                 'DESCRIPTION' => $e->getMessage()
             ]);
         }
+
+        // try {
+
+        //     $setDate = DB::raw('CURDATE()');
+
+        //     $query = DB::table('credit_schedule')
+        //         ->where('PAYMENT_DATE', '<=', $setDate)
+        //         ->where(function ($query) {
+        //             $query->whereNull('PAID_FLAG')
+        //                 ->orWhere('PAID_FLAG', '=', '');
+        //         })
+        //         ->select('*')
+        //         ->get();
+
+        //     if ($query->isEmpty()) {
+        //         M_CronJobLog::create([
+        //             'STATUS' => 'SUCCESS',
+        //             'DESCRIPTION' => 'DATA ARREARS EMPTY'
+        //         ]);
+        //         return;
+        //     }
+
+        //     $arrearsData = [];
+        //     foreach ($query as $result) {
+        //         $date = date('Y-m-d');
+        //         $daysDiff = (strtotime($date) - strtotime($result->PAYMENT_DATE)) / (60 * 60 * 24);
+        //         $pastDuePenalty = $result->INSTALLMENT * ($daysDiff * 0.005);
+
+        //         $arrearsData[] = [
+        //             'ID' => Uuid::uuid7()->toString(),
+        //             'STATUS_REC' => 'A',
+        //             'LOAN_NUMBER' => $result->LOAN_NUMBER,
+        //             'START_DATE' => $result->PAYMENT_DATE,
+        //             'END_DATE' => null,
+        //             'PAST_DUE_PCPL' => $result->PRINCIPAL ?? 0,
+        //             'PAST_DUE_INTRST' => $result->INTEREST ?? 0,
+        //             'PAST_DUE_PENALTY' => $pastDuePenalty ?? 0,
+        //             'PAID_PCPL' => $result->PAYMENT_VALUE_PRINCIPAL ?? 0,
+        //             'PAID_INT' => $result->PAYMENT_VALUE_INTEREST ?? 0,
+        //             'PAID_PENALTY' => 0,
+        //             'CREATED_AT' => Carbon::now('Asia/Jakarta')
+        //         ];
+        //     }
+
+        //     foreach ($arrearsData as $data) {
+        //         $existingArrears = M_Arrears::where([
+        //             'LOAN_NUMBER' => $data['LOAN_NUMBER'],
+        //             'START_DATE' => $data['START_DATE'],
+        //             'STATUS_REC' => 'A'
+        //         ])->first();
+
+        //         if ($existingArrears) {
+        //             // Update the existing record
+        //             $existingArrears->update([
+        //                 'PAST_DUE_PENALTY' => $data['PAST_DUE_PENALTY'] ?? 0,
+        //                 'UPDATED_AT' => Carbon::now('Asia/Jakarta')
+        //             ]);
+        //         } else {
+        //             // Insert new record
+        //             M_Arrears::create($data);
+        //         }
+        //     }
+
+        //     DB::table('first_arr')->delete();
+
+        //     $data = DB::table('arrears')
+        //         ->selectRaw('LOAN_NUMBER, min(START_DATE) as start_date, datediff(now(), min(START_DATE)) as date_diff')
+        //         ->where('status_rec', 'A')
+        //         ->groupBy('LOAN_NUMBER')
+        //         ->get();
+
+        //     foreach ($data as $row) {
+        //         M_FirstArr::create([
+        //             'LOAN_NUMBER' => $row->LOAN_NUMBER,
+        //             'START_DATE' => $row->start_date,
+        //             'DATE_DIFF' => $row->date_diff
+        //         ]);
+        //     }
+
+        //     M_CronJobLog::create([
+        //         'STATUS' => 'SUCCESS',
+        //         'DESCRIPTION' => 'Records processed successfully'
+        //     ]);
+        // } catch (\Exception $e) {
+        //     M_CronJobLog::create([
+        //         'STATUS' => 'FAIL',
+        //         'DESCRIPTION' => $e->getMessage()
+        //     ]);
+        // }
     }
 }
