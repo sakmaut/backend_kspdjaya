a10609cbf01c015c9e816a345ab4ed9f87c43812
alex <alexanderlieepm@gmail.com>
2025-02-28 13:23:22 +0700

    ok

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -61,28 +61,28 @@ class Welcome extends Controller
 
         $type = $data->INSTALLMENT_TYPE;
 
-        if (strtolower($type) == 'bulanan') {
-            $data_credit_schedule = $this->generateAmortizationSchedule($set_tgl_awal, $data);
-        } else {
-            $data_credit_schedule = $this->generateAmortizationScheduleMusiman($set_tgl_awal, $data);
-        }
+        // if (strtolower($type) == 'bulanan') {
+        //     $data_credit_schedule = $this->generateAmortizationSchedule($set_tgl_awal, $data);
+        // } else {
+        //     $data_credit_schedule = $this->generateAmortizationScheduleMusiman($set_tgl_awal, $data);
+        // }
 
-        $no = 1;
-        foreach ($data_credit_schedule as $list) {
-            $credit_schedule =
-                [
-                    'ID' => Uuid::uuid7()->toString(),
-                    'LOAN_NUMBER' => $request->loan_number ?? '',
-                    'INSTALLMENT_COUNT' => $no++,
-                    'PAYMENT_DATE' => parseDatetoYMD($list['tgl_angsuran']),
-                    'PRINCIPAL' => $list['pokok'],
-                    'INTEREST' => $list['bunga'],
-                    'INSTALLMENT' => $list['total_angsuran'],
-                    'PRINCIPAL_REMAINS' => $list['baki_debet']
-                ];
+        // $no = 1;
+        // foreach ($data_credit_schedule as $list) {
+        //     $credit_schedule =
+        //         [
+        //             'ID' => Uuid::uuid7()->toString(),
+        //             'LOAN_NUMBER' => $request->loan_number ?? '',
+        //             'INSTALLMENT_COUNT' => $no++,
+        //             'PAYMENT_DATE' => parseDatetoYMD($list['tgl_angsuran']),
+        //             'PRINCIPAL' => $list['pokok'],
+        //             'INTEREST' => $list['bunga'],
+        //             'INSTALLMENT' => $list['total_angsuran'],
+        //             'PRINCIPAL_REMAINS' => $list['baki_debet']
+        //         ];
 
-            M_CreditSchedule::create($credit_schedule);
-        }
+        //     M_CreditSchedule::create($credit_schedule);
+        // }
 
         $check_exist = M_Credit::where('ORDER_NUMBER', $request->order_number)->first();
 
@@ -92,7 +92,7 @@ class Welcome extends Controller
 
             $this->insert_customer($request, $data, $cust_code);
             $this->insert_customer_xtra($data, $cust_code);
-            $this->insert_collateral($request, $data, $SET_UUID, $request->loan_number);
+            // $this->insert_collateral($request, $data, $SET_UUID, $request->loan_number);
         }
     }
 
