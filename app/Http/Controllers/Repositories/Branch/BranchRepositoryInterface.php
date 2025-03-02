31bcec72848d88fee59ab9c8856e6b0f7e48e9db
alex <alexanderlieepm@gmail.com>
2025-03-02 07:19:44 +0700

    up

--- app/Http/Controllers/Repositories/Branch/BranchRepositoryInterface.php
+++ app/Http/Controllers/Repositories/Branch/BranchRepositoryInterface.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Repositories\Branch;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
 interface BranchRepositoryInterface
 {
     function getActiveBranch();
