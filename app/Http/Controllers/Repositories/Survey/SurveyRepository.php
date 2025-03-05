dd713c46231117d7793ee7bf551ca89067a99cb5
alex <alexanderlieepm@gmail.com>
2025-03-05 13:29:18 +0700

    up

---
+++ app/Http/Controllers/Repositories/Survey/SurveyRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Repositories\Survey;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SurveyRepository implements SurveyInterface
+{
+    //
+}
