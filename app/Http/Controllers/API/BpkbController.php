283e195ff71e74093ef6f3b79f4790bbe14a98bc
alex <alexanderlieepm@gmail.com>
2025-03-07 23:03:13 +0700

    ok

--- app/Http/Controllers/API/BpkbController.php
+++ app/Http/Controllers/API/BpkbController.php
@@ -38,7 +38,7 @@ class BpkbController extends Controller
             $branch = $request->user()->branch_id;
             $position = $request->user()->position;
 
-            $dto = DB::select(DB::raw("SELECT * FROM collateral_view"));
+            $dto = DB::table('collateral_view')->get();
 
             return response()->json($dto, 200);
         } catch (\Exception $e) {
