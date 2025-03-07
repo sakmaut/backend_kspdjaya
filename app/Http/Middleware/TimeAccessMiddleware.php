ddc66196530c48256b60e8cb76f1c656e4f2476f
alex <alexanderlieepm@gmail.com>
2025-03-07 19:38:42 +0700

    pindah

--- app/Http/Middleware/TimeAccessMiddleware.php
+++ app/Http/Middleware/TimeAccessMiddleware.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Carbon\Carbon;
 use Closure;
-use Exception;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,17 +16,12 @@ class TimeAccessMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        try {
-            $currentTime = Carbon::now()->format('H');
+        $currentTime = Carbon::now()->format('H');
 
-            // if ($currentTime >= 23 || $currentTime < 3) {
-            if ($currentTime >= 23 || $currentTime < 3) {
-                throw new Exception("Akses API dibatasi pada jam 11 malam hingga 3 pagi.", 503);
-            }
-
-            return $next($request);
-        } catch (\Throwable $e) {
-            return response()->json($e->getMessage(),503);
+        if ($currentTime >= 23 || $currentTime < 3) {
+            abort(403, 'Akses API dibatasi pada jam 11 malam hingga 3 pagi.');
         }
+        
+        return $next($request);
     }
 }
