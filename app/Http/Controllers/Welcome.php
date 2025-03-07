dc1b8da8acec6ca4274db95dbb2714d5f27d6f41
alex <alexanderlieepm@gmail.com>
2025-03-07 19:24:44 +0700

    ss

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -62,11 +62,11 @@ class Welcome extends Controller
 
                 $setUrl = "https://los.kspdjaya.id";
 
-                if ($request->getBaseUrl() == $setUrl || $currentTime >= 23 || $currentTime < 3) {
+                if ($request->getSchemeAndHttpHost() == $setUrl || $currentTime >= 23 || $currentTime < 3) {
                     throw new Exception("Akses API dibatasi pada jam 11 malam hingga 3 pagi.", 503);
                 }
 
-                return response()->json("OK MASUK " . $request->getBaseUrl(), 200);
+                return response()->json("OK MASUK " . $request->getSchemeAndHttpHost(), 200);
             } catch (\Throwable $e) {
                 return response()->json($e->getMessage(), 503);
             }
