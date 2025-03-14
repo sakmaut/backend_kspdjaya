049613a14dca1992bdb9a26668af3a3857eeaf98
alex <alexanderlieepm@gmail.com>
2025-03-14 14:03:57 +0700

    ss

--- app/Http/Controllers/API/BpkbTransactionController.php
+++ app/Http/Controllers/API/BpkbTransactionController.php
@@ -389,7 +389,6 @@ class BpkbTransactionController extends Controller
                 if (!empty($collateralIds)) {
                     M_CrCollateral::whereIn('ID', $result['kendaraan'])
                         ->update([
-                            'LOCATION_BRANCH' => $request->user()->branch_id ?? '',
                             'STATUS' => 'REQUEST'
                         ]);
                 }
