f7405df1fcd4939372804237f234f3f294e6ce97
alex <alexanderlieepm@gmail.com>
2025-03-26 13:36:04 +0700

    dd

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -978,19 +978,21 @@ class ListBanController extends Controller
 
             if ($getNow == $dateFrom) {
 
-                $checkRunSp = DB::select(" SELECT 
-                                                CASE 
-                                                    WHEN (SELECT MAX(p.ENTRY_DATE) FROM payment p) > (SELECT MAX(temp_lis_02C.last_pay) FROM temp_lis_02C) 
-                                                        AND job_status = 0 THEN 'run' 
-                                                    ELSE 'skip' 
-                                                END AS execute_sp
-                                            FROM job_on_progress
-                                            WHERE job_name = 'LISBAN'
-                                        ");
+                // $checkRunSp = DB::select(" SELECT 
+                //                                 CASE 
+                //                                     WHEN (SELECT MAX(p.ENTRY_DATE) FROM payment p) > (SELECT MAX(temp_lis_02C.last_pay) FROM temp_lis_02C) 
+                //                                         AND job_status = 0 THEN 'run' 
+                //                                     ELSE 'skip' 
+                //                                 END AS execute_sp
+                //                             FROM job_on_progress
+                //                             WHERE job_name = 'LISBAN'
+                //                         ");
 
-                if (!empty($checkRunSp) && $checkRunSp[0]->execute_sp === 'run') {
-                    DB::select('CALL lisban_berjalan(?)', [$getNow]);
-                }
+                // if (!empty($checkRunSp) && $checkRunSp[0]->execute_sp === 'run') {
+                //     DB::select('CALL lisban_berjalan(?)', [$getNow]);
+                // }
+
+                DB::select('CALL lisban_berjalan(?)', [$getNow]);
 
                 $query = $query2;
             } else {
