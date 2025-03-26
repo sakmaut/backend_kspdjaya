c91a3a99fb31cdfefd3866da8ee8b9be784f0950
alex <alexanderlieepm@gmail.com>
2025-03-26 14:53:05 +0700

    fff

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -768,9 +768,9 @@ class ListBanController extends Controller
             $getBranch = $request->cabang_id;
             $getPosition = $request->position;
 
-            $getBranchCode = M_Branch::find($getBranch);
+            $getBranchCode = $request->cabang_id;
 
-            return response()->json('CABANG ' . $getBranchCode->CODE_NUMBER, 200);
+            return response()->json('CABANG ' . $getBranch, 200);
 
             // if ($getPosition != 'HO') {
             //     return response()->json('YA', 200);
