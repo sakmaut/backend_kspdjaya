232246a4fbe4255aec1acc9196ce7674a978000b
alex <alexanderlieepm@gmail.com>
2025-03-26 09:04:26 +0700

    ok

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -969,7 +969,21 @@ class ListBanController extends Controller
             $getNow = date('mY', strtotime(now()));
 
             if ($getNow == $dateFrom) {
-                DB::select('CALL lisban_berjalan(?)', [$getNow]);
+
+                $checkRunSp = DB::select(" SELECT 
+                                                CASE 
+                                                    WHEN (SELECT MAX(p.ENTRY_DATE) FROM payment p) > (SELECT MAX(temp_lis_02C.last_pay) FROM temp_lis_02C) 
+                                                        AND job_status = 0 THEN 'run' 
+                                                    ELSE 'skip' 
+                                                END AS execute_sp
+                                            FROM job_on_progress
+                                            WHERE job_name = 'LISBAN'
+                                        ");
+
+                if (!empty($checkRunSp) && $checkRunSp[0]->execute_sp === 'run') {
+                    DB::select('CALL lisban_berjalan(?)', [$getNow]);
+                }
+
                 $query = $query2;
             } else {
                 $query = $query1;
