4465e7b6b7ddd0e24ce69411f470910856bb5878
alex <alexanderlieepm@gmail.com>
2025-04-10 09:54:13 +0700

    dd

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -1025,7 +1025,7 @@ class ListBanController extends Controller
             if (strtolower($getPosition) != 'ho') {
                 $query .= " AND cl.BRANCH = '$getBranchIdUser'";
             } else {
-                if (!empty($getBranch) && ($getBranch != 'SEMUA CABANG' || $getBranch != '8593fd4e-b54e-11ef-97d5-bc24112eb731')) {
+                if (!empty($getBranch) && $getBranch != 'SEMUA CABANG' && $getBranch != '8593fd4e-b54e-11ef-97d5-bc24112eb731') {
                     $query .= " AND cl.BRANCH = '$getBranch'";
                 }
             }
