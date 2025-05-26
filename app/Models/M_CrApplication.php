aed97eb08298f210d38285e3d3cf30c395ffe5ac
alexlie94 <alexanderlieepm@gmail.com>
2025-05-26 22:52:59 +0700

    up

--- app/Models/M_CrApplication.php
+++ app/Models/M_CrApplication.php
@@ -114,7 +114,8 @@ class M_CrApplication extends Model
             ->join('branch as t3', 't3.ID', '=', 't1.BRANCH')
             ->join('users as t4', 't4.id', '=', 't2.created_by')
             ->join('application_approval as t6', 't6.cr_application_id', '=', 't1.ID')
-            ->join('cr_personal as t7', 't7.APPLICATION_ID', '=', 't1.ID');
+            ->join('cr_personal as t7', 't7.APPLICATION_ID', '=', 't1.ID')
+            ->join('survey_approval as t8', 't8.CR_SURVEY_ID', '=', 't2.id');
 
         if ($getPosition !== 'HO') {
             $query->where('t1.BRANCH', $getBranch);
@@ -125,7 +126,7 @@ class M_CrApplication extends Model
             foreach ($params as $param) {
                 $statuses[] = $param;
             }
-            $query->whereIn('t6.code', $statuses);
+            $query->whereIn('t8.CODE', $statuses);
         }
 
         $query->groupBy(
