619b047e195c3b3bda1d0232d09944af8eb345a4
alex <alexanderlieepm@gmail.com>
2025-06-02 16:20:26 +0700

    up

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -346,22 +346,22 @@ class ListBanController extends Controller
 
             if ($checkConditionDate) {
 
-                // $checkRunSp = DB::select(" SELECT
-                //                                 CASE
-                //                                     WHEN (SELECT MAX(p.ENTRY_DATE) FROM payment p) > (SELECT MAX(temp_lis_02C.last_pay) FROM temp_lis_02C)
-                //                                         AND job_status = 0 THEN 'run'
-                //                                     ELSE 'skip'
-                //                                 END AS execute_sp
-                //                             FROM job_on_progress
-                //                             WHERE job_name = 'LISBAN'");
-
                 $checkRunSp = DB::select(" SELECT
                                                 CASE
-                                                    WHEN job_status = 0 THEN 'run'
+                                                    WHEN (SELECT MAX(p.ENTRY_DATE) FROM payment p) >= (SELECT MAX(temp_lis_02C.last_pay) FROM temp_lis_02C)
+                                                        AND job_status = 0 THEN 'run'
                                                     ELSE 'skip'
                                                 END AS execute_sp
                                             FROM job_on_progress
                                             WHERE job_name = 'LISBAN'");
+
+                // $checkRunSp = DB::select(" SELECT
+                //                                 CASE
+                //                                     WHEN job_status = 0 THEN 'run'
+                //                                     ELSE 'skip'
+                //                                 END AS execute_sp
+                //                             FROM job_on_progress
+                //                             WHERE job_name = 'LISBAN'");
 
                 if (!empty($checkRunSp) && $checkRunSp[0]->execute_sp === 'run') {
                     DB::select('CALL lisban_berjalan(?,?)', [$getNow, $getUserName]);
