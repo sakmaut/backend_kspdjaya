84ed02360cbc2f8670595e43baacf1964d01228f
alex <alexanderlieepm@gmail.com>
2025-06-03 10:38:35 +0700

    update diskon payment not

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -234,9 +234,11 @@ class PaymentController extends Controller
             $this->updateArrears($loan_number, $tgl_angsuran, $res, $uid);
         }
 
-        if ($res['bayar_angsuran'] != 0 || $res['bayar_denda'] != 0 || (isset($res['diskon_denda']) && $res['diskon_denda'] == 1)) {
-            $this->createPaymentRecords($request, $res, $tgl_angsuran, $loan_number, $no_inv, $getCodeBranch, $uid);
-        }
+        // if ($res['bayar_angsuran'] != 0 || $res['bayar_denda'] != 0 || (isset($res['diskon_denda']) && $res['diskon_denda'] == 1)) {
+        //     $this->createPaymentRecords($request, $res, $tgl_angsuran, $loan_number, $no_inv, $getCodeBranch, $uid);
+        // }
+
+        $this->createPaymentRecords($request, $res, $tgl_angsuran, $loan_number, $no_inv, $getCodeBranch, $uid);
 
         $this->updateCredit($loan_number);
     }
