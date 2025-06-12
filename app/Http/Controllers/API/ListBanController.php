d3d37a1402fb6198a3f3503b947b28cc1d7946a1
alex <alexanderlieepm@gmail.com>
2025-06-12 14:56:30 +0700

    up

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -132,7 +132,7 @@ class ListBanController extends Controller
             $checkQueue = DB::table('job_on_progress')->where('JOB_NAME', $jobName)->first();
 
             $lastCallTime = Carbon::parse($checkQueue->LAST_CALL);
-            if ($checkQueue->JOB_STATUS == 1 || $lastCallTime->diffInMinutes(now()) > 5) {
+            if ($checkQueue->JOB_STATUS == 1 || $lastCallTime->diffInMinutes(now()) < 5) {
                 throw new Exception("RUNNING JOB", 408);
             }
 
