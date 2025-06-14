25e309e45574f33aec8e3a9add08b2e89861bb52
Alexander <74333486+alexlie94@users.noreply.github.com>
2025-06-14 17:59:57 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('auth/login', [AuthController::class, 'login'])->middleware('time.access')->name('login');
 Route::get('auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('welcome', [Welcome::class, 'index']);
+Route::get('welcome', [Welcome::class, 'index']);
 Route::get('jobArrears', [Welcome::class, 'job']);
 Route::post('botpel', [PelunasanController2::class, 'getDetail']);
 Route::post('propel', [PelunasanController2::class, 'propel']);
