0a7d7a6fe2258149e8e9822422d69320cffef3c1
alex <alexanderlieepm@gmail.com>
2025-07-03 13:13:46 +0700

    up

--- app/Http/Controllers/Enum/UserPosition/UserPositionEnum.php
+++ app/Http/Controllers/Enum/UserPosition/UserPositionEnum.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers\Enum\UserPosition;
 class UserPositionEnum
 {
     public const MCF = 'MCF';
+    public const KOLEKTOR = 'KOLEKTOR';
     public const ADMIN = 'ADMIN';
     public const KAPOS = 'KAPOS';
     public const HO = 'HO';
     public const SUPERADMIN = 'SUPERADMIN';
+
+    public function checkUserPositionPayment($request)
+    {
+        $getCurrentPosition = $request->user()->position;
+        $setPositionAvailable  = [$this::MCF, $this::KOLEKTOR];
+
+        return in_array($getCurrentPosition, $setPositionAvailable);
+    }
 }
