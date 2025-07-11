8d3cb650c0b5360d10c44f85f2a9f5f49a8c2d16
it-invent <alexanderlieepm@gmail.com>
2025-07-11 11:44:32 +0700

    dd

--- app/Http/Controllers/API/Wilayah.php
+++ app/Http/Controllers/API/Wilayah.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\R_Wilayah;
+use App\Models\M_KodePos;
 use App\Models\M_Wilayah;
 use Illuminate\Http\Request;
 
@@ -28,7 +29,7 @@ class Wilayah extends Controller
 
     public function kecamatan($id)
     {
-        $query = M_Wilayah::whereRaw('LENGTH(kode) = 8')->where('kode', 'like', '%' . $id . '%')->get();
+        $query = M_Wilayah::whereRaw('LENGTH(kode) = 8')->where('kode', 'like', '%' . 11.05 . '%')->get();
         $data = R_Wilayah::collection($query);
 
         return \response()->json($data);
@@ -40,5 +41,12 @@ class Wilayah extends Controller
         $data = R_Wilayah::collection($query);
 
         return \response()->json($data);
+    }
+
+    public function kode_pos($id)
+    {
+        $query = M_KodePos::where('kode', $id)->get();
+
+        return \response()->json($query);
     }
 }
