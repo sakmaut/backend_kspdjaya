b8d28e6016fee06f2cc357cd086284d8f6bf2b2d
it-invent <alexanderlieepm@gmail.com>
2025-07-11 11:05:10 +0700

    up

--- app/Http/Controllers/API/Wilayah.php
+++ app/Http/Controllers/API/Wilayah.php
@@ -11,7 +11,7 @@ class Wilayah extends Controller
 
     public function index()
     {
-        $query = M_Wilayah::whereRaw('LENGTH(code) = 2')->get();
+        $query = M_Wilayah::whereRaw('LENGTH(kode) = 2')->get();
         return $query;
     }
 }
