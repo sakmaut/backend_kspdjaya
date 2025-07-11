9765d31380a0c4cec39d08a94be232614133bb7b
it-invent <alexanderlieepm@gmail.com>
2025-07-11 11:22:22 +0700

    up

--- app/Http/Controllers/API/Wilayah.php
+++ app/Http/Controllers/API/Wilayah.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\R_Wilayah;
 use App\Models\M_Wilayah;
 use Illuminate\Http\Request;
 
@@ -11,24 +12,33 @@ class Wilayah extends Controller
     public function provinsi()
     {
         $query = M_Wilayah::whereRaw('LENGTH(kode) = 2')->get();
-        return $query;
+
+        $data = R_Wilayah::collection($query);
+
+        return \response()->json($data);
     }
 
     public function kota(Request $request)
     {
         $query = M_Wilayah::whereRaw('LENGTH(kode) = 5')->where('kode', 'like', '%' . $request->kode . '%')->get();
-        return $query;
+        $data = R_Wilayah::collection($query);
+
+        return \response()->json($data);
     }
 
     public function kelurahan(Request $request)
     {
         $query = M_Wilayah::whereRaw('LENGTH(kode) = 8')->where('kode', 'like', '%' . $request->kode . '%')->get();
-        return $query;
+        $data = R_Wilayah::collection($query);
+
+        return \response()->json($data);
     }
 
     public function kecamatan(Request $request)
     {
         $query = M_Wilayah::whereRaw('LENGTH(kode) = 11')->where('kode', 'like', '%' . $request->kode . '%')->get();
-        return $query;
+        $data = R_Wilayah::collection($query);
+
+        return \response()->json($data);
     }
 }
