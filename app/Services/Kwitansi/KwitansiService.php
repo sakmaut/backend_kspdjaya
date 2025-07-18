a4e5a53b400178666f9b8358e8cc13be64a9ee14
it-invent <alexanderlieepm@gmail.com>
2025-07-18 13:47:25 +0700

    ss

--- app/Services/Kwitansi/KwitansiService.php
+++ app/Services/Kwitansi/KwitansiService.php
@@ -33,6 +33,9 @@ class KwitansiService
         $user = $request->user();
         $url = url('/');
 
+        return ['url' => $url, 'cek' => $url != 'https://dapur.kspdjaya.id'];
+        die;
+
         if ($user->position === $this->userPositionEnum::HO && $url != 'https://dapur.kspdjaya.id') {
             return $this->kwitansiRepository->getPendingForHO();
         }
