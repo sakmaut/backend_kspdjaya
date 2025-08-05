c43a1785a2757e443cc981135fb3fbd8ce282d1f
it-invent <alexanderlieepm@gmail.com>
2025-08-05 14:50:18 +0700

    ototo

--- app/Http/Controllers/API/CrAppilcationController.php
+++ app/Http/Controllers/API/CrAppilcationController.php
@@ -1496,15 +1496,15 @@ class CrAppilcationController extends Controller
             ])
             ->orderByDesc('credit.CREATED_AT');
 
-        if (!empty($loan_number)) {
+        if (!empty($loan_number) || $loan_number != 'undefined') {
             $query->where('credit.LOAN_NUMBER', $loan_number);
         }
 
-        if (!empty($atas_nama)) {
+        if (!empty($atas_nama) || $atas_nama != 'undefined') {
             $query->where('customer.NAME', 'like', "%$atas_nama%");
         }
 
-        if (!empty($cabang)) {
+        if (!empty($cabang) || $cabang != 'undefined') {
             $query->where('branch.CODE', $cabang);
         }
 
