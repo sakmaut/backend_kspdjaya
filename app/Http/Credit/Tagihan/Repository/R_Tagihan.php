f685da4350b75112cb59ed813b0ebe7af628d3f1
it-invent <alexanderlieepm@gmail.com>
2025-09-19 10:24:18 +0700

    UP

--- app/Http/Credit/Tagihan/Repository/R_Tagihan.php
+++ app/Http/Credit/Tagihan/Repository/R_Tagihan.php
@@ -107,42 +107,20 @@ class R_Tagihan
                                 cl.CUST_CODE,
                                 tg.NO_SURAT,
                                 us.fullname AS username,
-                                case 
-                                    when 
-                                        (
-                                            date_format(cl.created_at,'%m%Y') = '$dateFrom'
-                                            or (cl.STATUS_REC = 'RP' and py.ID is null)
-                                            or replace(format(
-                                                    case 
-                                                        when date_format(cl.created_at,'%m%Y') = '$dateFrom' 
-                                                        then (cl.PCPL_ORI + cl.INTRST_ORI) 
-                                                        else (st.init_pcpl + st.init_int) 
-                                                    end, 0), ',', '') = '0'
-                                            or (
-                                                case 
-                                                    when (cl.INSTALLMENT_COUNT / cl.PERIOD) = 1 then 'REGULER' 
-                                                    else 'MUSIMAN' 
-                                                end = 'MUSIMAN'
-                                                and date_format(st.first_arr,'%m%Y') = date_format(date_add(str_to_date(concat('01','$dateFrom'),'%d%m%Y'), interval 1 month), '%m%Y')
-                                            )
-                                            or st.first_arr >= date_add(str_to_date(concat('01','$dateFrom'),'%d%m%Y'), interval 2 month)
-                                            or (
-                                                st.first_arr > date_add(date_add(str_to_date(concat('01','$dateFrom'),'%d%m%Y'), interval 1 month), interval -1 day)
-                                                and case 
-                                                        when (cl.INSTALLMENT_COUNT / cl.PERIOD) = 1 then 'REGULER' 
-                                                        else 'MUSIMAN' 
-                                                    end = 'REGULER'
-                                            )
-                                            or st.arr_count > 8
-                                            or st.arr_count is not null
-                                        )
-                                        and
-                                        (
-                                            date_format(cl.created_at,'%m%Y') = '$dateFrom'
-                                            or coalesce(st.first_arr, en.first_arr) is not null
-                                        )
-                                    then 'YA'
-                                    else 'TIDAK'
+                                case    when (	concat('C',case when date_format(cl.created_at,'%m%Y')='$dateFrom' then 'N'
+                                        when cl.STATUS_REC = 'RP' and py.ID is null and date_format(col.SITA_AT,'%m%Y')<>'$dateFrom'  then 'L'
+                                        when replace(format(case when date_format(cl.created_at,'%m%Y')='$dateFrom' then (cl.PCPL_ORI+cl.INTRST_ORI)
+                                                            else (st.init_pcpl+st.init_int) end,0),',','')=0 then 'L'
+                                        when case when (cl.INSTALLMENT_COUNT/cl.PERIOD)=1 then 'REGULER' else 'MUSIMAN' end = 'MUSIMAN'
+                                                and date_format(st.first_arr,'%m%Y')=date_format(date_add(str_to_date(concat('01','$dateFrom'),'%d%m%Y'),interval 1 month),'%m%Y') then 'N'
+                                        when st.first_arr>=date_add(str_to_date(concat('01','$dateFrom'),'%d%m%Y'),interval 2 month) then 'N'
+                                        when st.first_arr > date_add(date_add(str_to_date(concat('01','$dateFrom'),'%d%m%Y'),interval 1 month),interval -1 day)
+                                                and case when (cl.INSTALLMENT_COUNT/cl.PERIOD)=1 then 'REGULER' else 'MUSIMAN' end = 'REGULER'  then 'M'
+                                        when st.arr_count > 8 then 'X'
+                                        else st.arr_count end)) in ('CM','C5','C4','C3','C2','C1','C0') 
+                                        and (case when date_format(cl.created_at,'%m%Y')='$dateFrom' then 1
+                                when coalesce(st.first_arr,en.first_arr) is null then ''
+                                else coalesce(st.last_inst,en.last_inst) end) in (1,2,3) then 'YA' else 'TIDAK' 
                                 end as nbot
                         FROM	credit cl
                                 inner join branch b on cast(b.ID as char) = cast(cl.BRANCH as char)
