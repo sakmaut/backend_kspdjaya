d48089e6bfb98dc088793594570097aa7b248eb4
it-invent <alexanderlieepm@gmail.com>
2025-09-19 10:37:45 +0700

    dd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -33,7 +33,7 @@ class C_Tagihan extends Controller
 
             $dto = R_TagihanDetail::collection($data);
 
-            return response()->json($dto, 200);
+            return response()->json("sss", 200);
         } catch (\Exception $e) {
             return $this->log->logError($e, $request);
         }
