36e75c302adf44464d19800f97b7b69b1138c6eb
it-invent <alexanderlieepm@gmail.com>
2025-10-07 11:45:30 +0700

    dd

--- app/Http/Controllers/API/ListBanController.php
+++ app/Http/Controllers/API/ListBanController.php
@@ -390,7 +390,7 @@ class ListBanController extends Controller
                 //                             FROM job_on_progress
                 //                             WHERE job_name = 'LISBAN'");
 
-                $branchCondition = empty($getBranch) && $getBranch == 'SEMUA CABANG' && $getBranch == 'semua';
+                $branchCondition = empty($getBranch) && ($getBranch == 'SEMUA CABANG' || $getBranch == 'semua');
                 $branchCon = $branchCondition ? '%' : $getBranch;
 
                 if (!empty($checkRunSp) && $checkRunSp[0]->execute_sp === 'run') {
