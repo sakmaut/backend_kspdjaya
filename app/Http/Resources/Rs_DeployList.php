a18e3b66c30602d04e43bbff1b0482bbef45abab
it-invent <alexanderlieepm@gmail.com>
2025-10-13 15:19:23 +0700

    ddd

--- app/Http/Resources/Rs_DeployList.php
+++ app/Http/Resources/Rs_DeployList.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -9,9 +10,11 @@ class Rs_DeployList extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $userName = User::where('username', $this->USER_ID)->first();
+
         return [
             'id' => $this->ID,
-            'nama_pic' => $this->USER_ID ?? "",
+            'nama_pic' =>  $userName->fullname ?? "",
             'no_kontrak' => $this->LOAN_NUMBER ?? "",
             'nama_customer' => $this->NAMA_CUST ?? "",
             'cycle_awal' => $this->CYCLE_AWAL ?? "",
