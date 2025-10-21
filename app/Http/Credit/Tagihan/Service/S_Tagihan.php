7e7b7cf26d4ba61d35809188f5e8f3ad05cd0190
it-invent <alexanderlieepm@gmail.com>
2025-10-21 09:31:15 +0700

    dd

--- app/Http/Credit/Tagihan/Service/S_Tagihan.php
+++ app/Http/Credit/Tagihan/Service/S_Tagihan.php
@@ -118,6 +118,7 @@ class S_Tagihan extends R_Tagihan
                 'MCF'           => $item['SURVEYOR'] ?? null,
                 'ANGSURAN_KE'   => $item['ANGS KE'] ?? 0,
                 'ANGSURAN'      => $item['ANGSURAN'] ?? 0,
+                'STATUS'        => "Aktif",
                 'CREATED_BY'    => $request->user()->id ?? null,
                 'CREATED_AT' => Carbon::now('Asia/Jakarta'),
             ];
