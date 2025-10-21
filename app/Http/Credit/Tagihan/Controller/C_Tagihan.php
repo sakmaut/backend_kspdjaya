d19c39aeb2136e4bf15d86da4cfe002c8da34d11
it-invent <alexanderlieepm@gmail.com>
2025-10-21 15:22:22 +0700

    ddd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -250,7 +250,6 @@ class C_Tagihan extends Controller
                     $join->on('e.REFERENCE_ID', '=', 'a.NO_SURAT');
                 })
                 ->where('a.USER_ID', $pic)
-                ->where('d.total_bayar', '<', 'a.ANGSURAN')
                 ->where(function ($query) {
                     $query->whereNull('c.LKP_NUMBER')
                         ->orWhere('c.LKP_NUMBER', '');
