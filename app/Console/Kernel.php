cd5556a20a845082e5c8acce71f7f0b04794fe03
it-invent <alexanderlieepm@gmail.com>
2025-10-23 11:33:07 +0700

    dd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('demo:cron')->dailyAt('00:05');
-        $schedule->command('app:saving-services')->dailyAt('11:30');
+        $schedule->command('app:saving-services')->dailyAt('11:34');
     }
 
     /**
