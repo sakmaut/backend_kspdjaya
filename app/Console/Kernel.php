34882bf0217017e8c506f486933468c690b78d65
it-invent <alexanderlieepm@gmail.com>
2025-10-23 08:48:53 +0700

    ddd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('demo:cron')->dailyAt('00:05');
         $schedule->command('app:last-monthly-credit-insert')->hourly();
         $schedule->command('app:lkp-service')->dailyAt('05:00');
-        $schedule->command('app:listan-service')->dailyAt('13:03');
+        $schedule->command('app:listan-service')->monthlyOn(1, '05:00');
     }
 
     protected function commands(): void
