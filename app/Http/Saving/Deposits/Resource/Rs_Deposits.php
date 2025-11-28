9f9627c307ea6ba1095d935eee0dd5312dc4a7d1
it-invent <alexanderlieepm@gmail.com>
2025-11-28 13:21:58 +0700

    ddd

--- app/Http/Saving/Deposits/Resource/Rs_Deposits.php
+++ app/Http/Saving/Deposits/Resource/Rs_Deposits.php
@@ -9,11 +9,25 @@ class Rs_Deposits extends JsonResource
 {
     public function toArray(Request $request): array
     {
+
+        // bunga kotor sebelum pajak
+        $bunga_kotor = $this->deposit_value * $this->int_rate * ($this->period / 12);
+
+        // pajak (20% dari bunga kotor)
+        $pajak = $bunga_kotor * 0.20;
+
+        // bunga bersih yg diterima
+        $bunga_bersih = $bunga_kotor - $pajak;
+
         return [
             "id" => $this->id,
             "no_deposito" => $this->deposit_number,
             "nama_pemilik" => $this->deposit_holder,
-            "alamat" => ""
+            "alamat" => "",
+            "nominal" => (int) ($this->deposit_value ?? 0),
+            "bunga" => (int) ($this->int_rate ?? 0),
+            "pajak" => $bunga_kotor,
+            "bunga_pajak" => $bunga_bersih
         ];
     }
 }
