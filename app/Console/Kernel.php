030184e0bb23844b2e7ac2fb0691ee3147ee39ee
it-invent <alexanderlieepm@gmail.com>
2025-12-01 13:07:00 +0700

    dd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,10 +21,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:lkp-service')->dailyAt('05:00')->timezone('Asia/Jakarta');
 
         // Running On First Month
-        // $schedule->command('app:monthly-run-sp')->monthlyOn(1, '02:00')->timezone('Asia/Jakarta');
+        $schedule->command('app:monthly-run-sp')->monthlyOn(1, '03:00')->timezone('Asia/Jakarta');
         $schedule->command('app:listan-service')->monthlyOn(1, '05:00')->timezone('Asia/Jakarta');
-
-        $schedule->command('app:monthly-run-sp')->dailyAt('12:59');
     }
 
     protected function commands(): void
