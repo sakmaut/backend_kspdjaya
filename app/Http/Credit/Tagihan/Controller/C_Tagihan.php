acada30c734fab08058ee1e013e9fe2f380e9f61
it-invent <alexanderlieepm@gmail.com>
2025-12-03 10:02:00 +0700

    sdsd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -75,8 +75,8 @@ class C_Tagihan extends Controller
                         ->whereYear('d.CREATED_AT', now()->year);
                 })
                 ->leftJoin('users as e', 'e.id', '=', 'a.SURVEYOR_ID')
-                ->whereIn('a.CYCLE_AWAL', $cycles);
-            // ->whereNull('d.LOAN_NUMBER');
+                ->whereIn('a.CYCLE_AWAL', $cycles)
+                ->whereNull('d.LOAN_NUMBER');
 
             if ($currentPosition != 'HO') {
                 $query->where('a.BRANCH_ID', $currentBranch);
