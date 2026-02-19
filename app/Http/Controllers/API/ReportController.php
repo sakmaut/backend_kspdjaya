7c3ad33d0bd9edfd14722cebdb5d5facb831eabe
it-invent <alexanderlieepm@gmail.com>
2026-02-19 15:20:13 +0700

    sss

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -533,16 +533,19 @@ class ReportController extends Controller
 
                     $uniqKey = $angs . '-' . $tglTempoFormatted;
 
+                    // ============================
+                    // FIRST ROW INSTALLMENT
+                    // ============================
                     $isFirstRow = !in_array($uniqKey, $usedTempoAngs);
-
-                    $displayDenda    = number_format($res->PAST_DUE_PENALTY ?? 0, 0);
 
                     if ($isFirstRow) {
 
                         $displayAngs     = $angs;
                         $displayTempo    = $tglTempoFormatted;
                         $displayAmtAngs  = number_format($amtAngs, 0);
+                        $displayDenda    = number_format($res->PAST_DUE_PENALTY ?? 0, 0);
 
+                        // Total hanya dihitung sekali
                         $ttlAmtAngs += $amtAngs;
                         $ttlDenda   += floatval($res->PAST_DUE_PENALTY ?? 0);
 
@@ -571,7 +574,7 @@ class ReportController extends Controller
                                 'Jt.Tempo'   => '',
                                 'Seq'        => '',
                                 'Amt Angs'   => '',
-                                'Denda'      => $displayDenda,
+                                'Denda'      => '',
                                 'No Invoice' => $res->INVOICE ?? '',
                                 'Tgl Bayar'  => $tglBayarFormatted,
                                 'Amt Bayar'  => number_format($amtBayar, 0),
