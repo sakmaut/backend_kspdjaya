7239e83c7c9f83cf0a7a13aa80a034ed670aedcb
it-invent <alexanderlieepm@gmail.com>
2026-02-25 10:45:54 +0700

    ddd

--- app/Http/Resources/R_CrProspect.php
+++ app/Http/Resources/R_CrProspect.php
@@ -22,111 +22,101 @@ class R_CrProspect extends JsonResource
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
-        $check_exist = M_Credit::where('ORDER_NUMBER', $this->order_number)->first();
-        $getApproval = M_SurveyApproval::where('CR_SURVEY_ID', $this->id)->first();
-
-        $type = empty($this->INSTALLMENT_TYPE) ? ($this->jenis_angsuran ?? '') : ($this->INSTALLMENT_TYPE ?? '');
-
-        switch (strtolower($type)) {
-            case 'bunga_menurun':
-                $jenis_angsuran =  'BUNGA MENURUN';
-                break;
-            case 'bulanan':
-                $jenis_angsuran =  'BULANAN';
-                break;
-            case 'musiman':
-                $jenis_angsuran =  'MUSIMAN';
-                break;
-            default:
-                $jenis_angsuran=  $type;
-                break;
-        }
-
-        $data = [
-            'id' => $this->id,
-            "flag" => !$check_exist ? 0 : 1,
-            "credit_id" => !$check_exist ? null : $check_exist->ID ?? null,
-            "jenis_angsuran" =>  $jenis_angsuran,
-            'order_number' => $this->order_number,
-            'visit_date' => $this->visit_date  == null ? null : date('d-m-Y', strtotime($this->visit_date)),
-            'nama_debitur' => $this->nama_debitur,
-            'alamat' => $this->alamat,
-            'hp' => $this->hp,
-            'plafond' => $this->plafond,
-            'status' => $getApproval ? $getApproval->APPROVAL_RESULT : '',
-            'status_code' => $getApproval ? $getApproval->CODE : '',
-            'attachment' => $this->attachment($this->id, "'sp', 'pk', 'dok'")
-        ];
-
-        return $data;
-    }
-
     // public function toArray(Request $request): array
     // {
-    //     $application = $this->cr_application;
-    //     $credit = $application?->credit;
-    //     $approval = $this->survey_approval;
+    //     $check_exist = M_Credit::where('ORDER_NUMBER', $this->order_number)->first();
+    //     $getApproval = M_SurveyApproval::where('CR_SURVEY_ID', $this->id)->first();
 
-    //     $type = $application->INSTALLMENT_TYPE
-    //         ?? $this->jenis_angsuran
-    //         ?? '';
+    //     $type = empty($this->INSTALLMENT_TYPE) ? ($this->jenis_angsuran ?? '') : ($this->INSTALLMENT_TYPE ?? '');
 
-    //     $jenis_angsuran = match (strtolower($type)) {
-    //         'bunga_menurun' => 'BUNGA MENURUN',
-    //         'bulanan' => 'BULANAN',
-    //         'musiman' => 'MUSIMAN',
-    //         default => $type
-    //     };
+    //     switch (strtolower($type)) {
+    //         case 'bunga_menurun':
+    //             $jenis_angsuran =  'BUNGA MENURUN';
+    //             break;
+    //         case 'bulanan':
+    //             $jenis_angsuran =  'BULANAN';
+    //             break;
+    //         case 'musiman':
+    //             $jenis_angsuran =  'MUSIMAN';
+    //             break;
+    //         default:
+    //             $jenis_angsuran=  $type;
+    //             break;
+    //     }
 
-    //     return [
+    //     $data = [
     //         'id' => $this->id,
-    //         "flag" => $credit ? 1 : 0,
-    //         "credit_id" => $credit?->ID,
-    //         "jenis_angsuran" => $jenis_angsuran,
-    //         'order_number' => $application?->ORDER_NUMBER,
-    //         'visit_date' => $this->visit_date ? Carbon::parse($this->visit_date)->format('d-m-Y') : null,
-    //         'nama_debitur' => $application?->cr_personal?->NAME ?? $this->nama,
+    //         "flag" => !$check_exist ? 0 : 1,
+    //         "credit_id" => !$check_exist ? null : $check_exist->ID ?? null,
+    //         "jenis_angsuran" =>  $jenis_angsuran,
+    //         'order_number' => $this->order_number,
+    //         'visit_date' => $this->visit_date  == null ? null : date('d-m-Y', strtotime($this->visit_date)),
+    //         'nama_debitur' => $this->nama_debitur,
     //         'alamat' => $this->alamat,
     //         'hp' => $this->hp,
-    //         'plafond' => $application?->SUBMISSION_VALUE ?? $this->plafond,
-    //         'status' => $approval?->APPROVAL_RESULT ?? '',
-    //         'status_code' => $approval?->CODE ?? '',
-    //         'attachment' =>  $this->attachment($this->id, "'sp', 'pk', 'dok'"),
+    //         'plafond' => $this->plafond,
+    //         'status' => $getApproval ? $getApproval->APPROVAL_RESULT : '',
+    //         'status_code' => $getApproval ? $getApproval->CODE : '',
+    //         'attachment' => $this->attachment($this->id, "'sp', 'pk', 'dok'")
     //     ];
+
+    //     return $data;
     // }
 
-    // public function latestAttachments()
+    // public function attachment($survey_id, $data)
     // {
-    //     return $this->cr_survey_document()
-    //         ->whereIn('TYPE', ['sp', 'pk', 'dok'])
-    //         ->whereIn(DB::raw('(TYPE, TIMEMILISECOND)'), function ($query) {
-    //             $query->selectRaw('TYPE, MAX(TIMEMILISECOND)')
-    //                 ->from('cr_survey_document')
-    //                 ->whereColumn('CR_SURVEY_ID', 'cr_survey.CR_SURVEY_ID')
-    //                 ->whereIn('TYPE', ['sp', 'pk', 'dok'])
-    //                 ->groupBy('TYPE');
-    //         })
-    //         ->orderByDesc('TIMEMILISECOND')
-    //         ->get();
+    //     $documents = DB::select(
+    //         "   SELECT *
+    //             FROM cr_survey_document AS csd
+    //             WHERE (TYPE, TIMEMILISECOND) IN (
+    //                 SELECT TYPE, MAX(TIMEMILISECOND)
+    //                 FROM cr_survey_document
+    //                 WHERE TYPE IN ($data)
+    //                     AND CR_SURVEY_ID = '$survey_id'
+    //                 GROUP BY TYPE
+    //             )
+    //             ORDER BY TIMEMILISECOND DESC"
+    //     );
+
+    //     return $documents;
     // }
 
-    public function attachment($survey_id, $data)
+    public function toArray(Request $request): array
     {
-        $documents = DB::select(
-            "   SELECT *
-                FROM cr_survey_document AS csd
-                WHERE (TYPE, TIMEMILISECOND) IN (
-                    SELECT TYPE, MAX(TIMEMILISECOND)
-                    FROM cr_survey_document
-                    WHERE TYPE IN ($data)
-                        AND CR_SURVEY_ID = '$survey_id'
-                    GROUP BY TYPE
-                )
-                ORDER BY TIMEMILISECOND DESC"
-        );
+        $application = $this->cr_application;
+        $credit = $application?->credit;
+        $approval = $this->survey_approval;
 
-        return $documents;
+        $type = $application->INSTALLMENT_TYPE
+            ?? $this->jenis_angsuran
+            ?? '';
+
+        $jenis_angsuran = match (strtolower($type)) {
+            'bunga_menurun' => 'BUNGA MENURUN',
+            'bulanan' => 'BULANAN',
+            'musiman' => 'MUSIMAN',
+            default => $type
+        };
+
+        return [
+            'id' => $this->id,
+            "flag" => $credit ? 1 : 0,
+            "credit_id" => $credit?->ID,
+            "jenis_angsuran" => $jenis_angsuran,
+            'order_number' => $application?->ORDER_NUMBER,
+            'visit_date' => $this->visit_date ? Carbon::parse($this->visit_date)->format('d-m-Y') : null,
+            'nama_debitur' => $application?->cr_personal?->NAME ?? $this->nama,
+            'alamat' => $this->alamat,
+            'hp' => $this->hp,
+            'plafond' => $application?->SUBMISSION_VALUE ?? $this->plafond,
+            'status' => $approval?->APPROVAL_RESULT ?? '',
+            'status_code' => $approval?->CODE ?? '',
+            'attachment' =>  $this->cr_survey_document
+                            ->whereIn('TYPE', ['sp', 'pk', 'dok'])
+                            ->sortByDesc('TIMEMILISECOND')
+                            ->groupBy('TYPE')
+                            ->map(fn($docs) => $docs->first())
+                            ->values(),
+        ];
     }
 }
