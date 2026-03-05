1d05616140f3de80d9d49a925bfce05f44476171
it-invent <alexanderlieepm@gmail.com>
2026-03-05 15:15:21 +0700

    ddd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -381,7 +381,7 @@ class C_Tagihan extends Controller
                 ->join('users', 'users.username', '=', 'cl_lkp.USER_ID')
                 ->whereMonth('cl_lkp.CREATED_AT', Carbon::now()->month)
                 ->whereYear('cl_lkp.CREATED_AT', Carbon::now()->year)
-                ->orderBy('cl_lkp.CREATED_AT', 'DESC')
+                ->orderByDesc('cl_lkp.CREATED_AT')
                 ->orderBy('users.fullname', 'ASC')
                 ->select([
                     'cl_lkp.ID',
