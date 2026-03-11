ad9aa749df2ce10892e1b984269462f5f147d640
it-invent <alexanderlieepm@gmail.com>
2026-03-11 09:34:30 +0700

    ddd

--- app/Models/M_CrApplication.php
+++ app/Models/M_CrApplication.php
@@ -73,7 +73,7 @@ class M_CrApplication extends Model
 
     public function cr_survey()
     {
-        return $this->hasOne(M_CrSurvey::class, 'CR_SURVEY_ID', 'id');
+        return $this->belongsTo(M_CrSurvey::class, 'CR_SURVEY_ID', 'id');
     }
 
     public function cr_personal()
