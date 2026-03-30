5ad0b1cb265685c4e82b2bf0f45bfa799ee628c1
it-invent <alexanderlieepm@gmail.com>
2026-03-30 14:43:04 +0700

    ddd

--- app/Http/Controllers/API/Credit.php
+++ app/Http/Controllers/API/Credit.php
@@ -1246,7 +1246,7 @@ class Credit extends Controller
 
             $credit = M_Credit::where('LOAN_NUMBER', $loanNumber)
                 ->where('STATUS', 'A')
-                ->whereDate('CREATED_AT', now()->toDateString())
+                // ->whereDate('CREATED_AT', now()->toDateString())
                 ->first();
 
             if (!$credit) {
