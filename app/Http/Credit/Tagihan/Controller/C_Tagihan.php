e145537734289ecc303d90a6f9d208354f642cc4
alex <invent.team@inventgraphic.com>
2026-04-03 16:36:47 +0700

    ddd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -413,6 +413,7 @@ class C_Tagihan extends Controller
                     $join->on('survey.REFERENCE_ID', '=', 'b.NO_SURAT')
                         ->on('survey.LKP_NUMBER', '=', 'c.LKP_NUMBER');
                 })
+                ->where('c.STATUS', '!=', 'Draft')
                 ->groupBy('b.LOAN_NUMBER', 'c.LKP_NUMBER', 'c.ID')
                 ->havingRaw('COUNT(DISTINCT b.NO_SURAT) > COUNT(DISTINCT survey.REFERENCE_ID)')
                 ->select('b.LOAN_NUMBER', 'c.LKP_NUMBER');
@@ -432,7 +433,7 @@ class C_Tagihan extends Controller
                 })
                 ->where('a.USER_ID', $pic)
                 ->whereRaw('a.AMBC_TOTAL_AWAL > COALESCE(pay.total_bayar, 0)')
-                // ->where('cr.STATUS_REC', 'AC')
+                ->where('cr.STATUS_REC', 'AC')
                 ->whereMonth('a.CREATED_AT', now()->month)
                 ->whereYear('a.CREATED_AT', now()->year)
                 ->where(function ($query) {
