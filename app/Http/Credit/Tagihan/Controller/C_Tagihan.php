ef10f489a34d6476cd2f608983d4f6db7ce057c8
it-invent <alexanderlieepm@gmail.com>
2026-04-06 15:49:45 +0700

    ddd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -105,7 +105,9 @@ class C_Tagihan extends Controller
             $branchId = $user->branch_id;
             $position = strtoupper($user->position);
 
-            $query = M_ColllectorVisits::with('credit.cr_application.cr_survey');
+            $query = M_ColllectorVisits::with([
+                'credit.cr_application.cr_survey.cr_survey_document'
+            ]);
 
             // if (in_array($position, ['KAPOS', 'ADMIN'])) {
             //     $query->where('BRANCH_ID', $branchId);
