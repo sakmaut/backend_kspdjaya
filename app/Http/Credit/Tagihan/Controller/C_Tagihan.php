d75e76e4a201859ac73359787be666606cb44756
it-invent <alexanderlieepm@gmail.com>
2026-04-06 10:15:26 +0700

    dd

--- app/Http/Credit/Tagihan/Controller/C_Tagihan.php
+++ app/Http/Credit/Tagihan/Controller/C_Tagihan.php
@@ -422,7 +422,10 @@ class C_Tagihan extends Controller
                     $join->on('survey.REFERENCE_ID', '=', 'b.NO_SURAT')
                         ->on('survey.LKP_NUMBER', '=', 'c.LKP_NUMBER');
                 })
-                // ->where('c.STATUS', '!=', 'Draft')
+                ->where(function ($q) {
+                    $q->whereNotNull('c.LKP_NUMBER')
+                        ->orWhere('c.STATUS', '!=', 'Draft');
+                })
                 ->groupBy('b.LOAN_NUMBER', 'c.LKP_NUMBER', 'c.ID')
                 ->havingRaw('COUNT(DISTINCT b.NO_SURAT) > COUNT(DISTINCT survey.REFERENCE_ID)')
                 ->select('b.LOAN_NUMBER', 'c.LKP_NUMBER');
