2f992883fb119d9dad9259f73d43cd84323f94c6
it-invent <alexanderlieepm@gmail.com>
2026-04-10 08:43:43 +0700

    ddd

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -12,11 +12,17 @@ class HandleCors
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, X-Requested-With, Authorization,multipart/form-data');
-        $response->header('Access-Control-Allow-Credentials', 'true');
-        $response->header('Access-Control-Max-Age', '86400');
+        // $response->header('Access-Control-Allow-Origin', '*');
+        // $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        // $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, X-Requested-With, Authorization,multipart/form-data');
+        // $response->header('Access-Control-Allow-Credentials', 'true');
+        // $response->header('Access-Control-Max-Age', '86400');
+
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, X-Requested-With, Authorization, multipart/form-data');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->headers->set('Access-Control-Max-Age', '86400');
 
         return $response;
     }
