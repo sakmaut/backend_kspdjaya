0e9a9d12f476d1f8783b3edbe4239e27b89def53
it-invent <alexanderlieepm@gmail.com>
2026-04-14 13:05:38 +0700

    dd

--- app/Http/Resources/Rs_CollectorList.php
+++ app/Http/Resources/Rs_CollectorList.php
@@ -37,10 +37,14 @@ class Rs_CollectorList extends JsonResource
             'cycle_awal' => $this->CYCLE_AWAL ?? null,
             'cycle_akhir' => $this->CYCLE_AKHIR ?? null,
             'kunjungan_terakhir' => collect($kunjunganTerakhir)->filter()->isEmpty() ? null : $kunjunganTerakhir,
-            'pembayaran' => [
-                'tgl_bayar' => $this->ENTRY_DATE ? Carbon::parse($this->ENTRY_DATE)->format('Y-m-d') : null,
-                'total_bayar' => $this->total_bayar ?? 0
-            ]
+            'hasil_kunjungan' => $this->DESCRIPTION ?? "",
+            'tgl_kunjungan' => $this->SURVEY_DATE
+                ? Carbon::parse($this->SURVEY_DATE)->format('Y-m-d')
+                : null,
+            'tgl_jb' => $this->CONFIRM_DATE ?? null,
+            'path' => $path,
+            'tgl_bayar' => $this->ENTRY_DATE ? Carbon::parse($this->ENTRY_DATE)->format('Y-m-d') : null,
+            'total_bayar' => $this->total_bayar ?? 0
         ];
     }
 }
