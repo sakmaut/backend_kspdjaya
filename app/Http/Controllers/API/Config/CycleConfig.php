a8fe9f3f2b8300247354c193e84e78a64881a0e5
it-invent <alexanderlieepm@gmail.com>
2026-06-18 09:38:31 +0700

    sdfdsf

--- app/Http/Controllers/API/Config/CycleConfig.php
+++ app/Http/Controllers/API/Config/CycleConfig.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\API\Config;
 
 class CycleConfig
 {
+    const BASE_CYCLES = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6'];
+
     public static function getAllowedListbanCycles()
     {
-        return ['C0','C1', 'C2', 'C3', 'C4', 'C5', 'C6','CM','CN'];
+        return [...self::BASE_CYCLES, 'CM', 'CN'];
     }
 
     public static function getAllowedDeployCycles()
     {
-        return ['C0','C1', 'C2', 'C3', 'C4', 'C5', 'C6'];
+        return self::BASE_CYCLES;
     }
 }
